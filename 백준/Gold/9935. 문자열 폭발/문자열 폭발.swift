cb37eed5495f9d279a733aa3ff2f084125d3fc56
Junhyuk Hur <xuzhunhe@gmail.com>
2023-06-19 10:52:49 +0900

    [Gold IV] Title: 문자열 폭발, Time: 132 ms, Memory: 94712 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.swift
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.swift
@@ -1,11 +1,21 @@
 let line = readLine()!
-let bomb = readLine()!
+let bomb = Array(readLine()!)
 let bombLength = bomb.count
 var answer = [Character]()
-for character in line {
-    answer.append(character)
-    while character == bomb.last! && answer.count >= bombLength && String(answer[answer.count - bombLength..<answer.count]) == bomb {
-        answer.removeLast(bombLength)
+for c in line {
+    answer.append(c)
+    if isDelete(answer, bomb) {
+        answer.removeLast(bomb.count)
     }
 }
 print(answer.isEmpty ? "FRULA" : String(answer))
+
+func isDelete(_ answer: [Character], _ bomb: [Character]) -> Bool {
+    if answer.count < bomb.count {
+        return false
+    }
+    for i in 1...bomb.count where answer[answer.count - i] != bomb[bomb.count - i] {
+        return false
+    }
+    return true
+}
