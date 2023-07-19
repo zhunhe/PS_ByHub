2116125c85697c3ed68ef2c12821e6f11867099a
Junhyuk Hur <xuzhunhe@gmail.com>
2023-07-19 20:51:23 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,57 +1,60 @@
-/*
- * 파티
- * https://www.acmicpc.net/problem/1238
- */
-
 #include <bits/stdc++.h>
 using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+#define INF 987654321
+
+struct Data {
+    int pos, cost;
+
+    bool operator<(const Data& rhs) const {
+        return cost > rhs.cost;
+    }
+};
+
+int n, m, x, from, to, cost;
+vector<Data> adj[1002];
+vector<Data> adj2[1002];
+priority_queue<Data> pq;
 
 int main() {
-	int n, m, x;	cin >> n >> m >> x;
-	vector<vector<pair<int, int>>> graph(n + 1);
-	vector<vector<pair<int, int>>> graph2(n + 1);
-	for (int i = 0; i < m; i++) {
-		int from, to, cost;	cin >> from >> to >> cost;
-		graph[from].push_back({cost, to});
-		graph2[to].push_back({cost, from});
-	}
-	vector<int> dist(m + 1, INT_MAX);
-	dist[x] = 0;
-	priority_queue<pair<int, int>> pq;
-	pq.push({0, x});
-	while (pq.size()) {
-		int now = pq.top().second;
-		int now_cost = -pq.top().first;
-		pq.pop();
-		for (auto x : graph[now]) {
-			int next = x.second;
-			int next_cost = now_cost + x.first;
-			if (next_cost >= dist[next]) continue;
-			dist[next] = next_cost;
-			pq.push({-next_cost, next});
-		}
-	}
+    FASTIO
+    cin >> n >> m >> x;
+    for (int i = 0; i < m; i++) {
+        cin >> from >> to >> cost;
+        adj[from].push_back({to, cost});
+        adj2[to].push_back({from, cost});
+    }
+    vector<int> dist(n + 1, INF);
+    dist[x] = 0;
+    pq.push({x, 0});
+    while (!pq.empty()) {
+        const Data now = pq.top(); pq.pop();
+        if (now.cost > dist[now.pos]) continue;
+        for (Data next : adj[now.pos]) {
+            if (now.cost + next.cost >= dist[next.pos]) continue;
+            dist[next.pos] = now.cost + next.cost;
+            pq.push({next.pos, dist[next.pos]});
+        }
+    }
 
-	vector<int> dist2(m + 1, INT_MAX);
-	dist2[x] = 0;
-	pq.push({0, x});
-	while (pq.size()) {
-		int now = pq.top().second;
-		int now_cost = -pq.top().first;
-		pq.pop();
-		for (auto x : graph2[now]) {
-			int next = x.second;
-			int next_cost = now_cost + x.first;
-			if (next_cost >= dist2[next]) continue;
-			dist2[next] = next_cost;
-			pq.push({-next_cost, next});
-		}
-	}
-	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		if (dist[i] != INT_MAX && dist2[i] != INT_MAX) {
-			ans = max(ans, dist[i] + dist2[i]);
-		}
-	}
-	cout << ans;
+    vector<int> dist2(n + 1, INF);
+    dist2[x] = 0;
+    pq.push({x, 0});
+    while (!pq.empty()) {
+        const Data now = pq.top(); pq.pop();
+        if (now.cost > dist2[now.pos]) continue;
+        for (Data next : adj2[now.pos]) {
+            if (now.cost + next.cost >= dist2[next.pos]) continue;
+            dist2[next.pos] = now.cost + next.cost;
+            pq.push({next.pos, dist2[next.pos]});
+        }
+    }
+
+    int answer = 0;
+    for (int i = 1; i < n + 1; i++) {
+        if (dist[i] == INF || dist2[i] == INF) continue;
+        answer = max(answer, dist[i] + dist2[i]);
+    }
+    cout << answer << '\n';
 }
