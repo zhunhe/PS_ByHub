89c99e7a8ea49dc8442fce3e5d50b3cc33657ef6
Junhyuk Hur <xuzhunhe@gmail.com>
2023-07-20 21:43:03 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 832 ms, Memory: 27592 KB -BaekjoonHub

---
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+struct Data {
+    int pos, cost;
+
+    bool operator<(const Data& rhs) const {
+        return cost > rhs.cost;
+    }
+};
+
+priority_queue<int> dist[1002];
+priority_queue<Data> pq;
+vector<Data> adj[1002];
+
+int main() {
+    FASTIO
+    int n, m, k; cin >> n >> m >> k;
+    for (int i = 0; i < m; i++) {
+        int from, to, cost; cin >> from >> to >> cost;
+        adj[from].push_back({to, cost});
+    }
+    dist[1].push(0);
+    pq.push({0, 1});
+    while (!pq.empty()) {
+        const Data now = pq.top(); pq.pop();
+        for (Data next : adj[now.cost]) {
+            if (dist[next.pos].size() < k || dist[next.pos].top() > now.pos + next.cost) {
+                dist[next.pos].push(now.pos + next.cost);
+                pq.push({now.pos + next.cost, next.pos});
+                if (dist[next.pos].size() > k) dist[next.pos].pop();
+            }
+        }
+    }
+    for (int i = 1; i < n + 1; i++) cout << (dist[i].size() < k ? -1 : dist[i].top()) << '\n';
+}
