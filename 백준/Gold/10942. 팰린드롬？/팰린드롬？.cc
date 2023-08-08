af3524ffbee2dbc3ee41fd8bc5e72f0586d4a85a
Junhyuk Hur <xuzhunhe@gmail.com>
2023-08-08 10:01:54 +0900

    [Gold IV] Title: 팰린드롬?, Time: 256 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -0,0 +1,30 @@
+/*
+ * 팰린드롬?
+ * https://www.acmicpc.net/problem/10942
+ */
+
+#include "bits/stdc++.h"
+using namespace std;
+#define MAXN	2001
+int n, seq[MAXN], cache[MAXN][MAXN];
+
+int dp(int s, int e) {
+	if (cache[s][e] != -1) return cache[s][e];
+	int &ret = cache[s][e];
+	if (s == e) return ret = 1;
+	else if (s + 1 == e) return ret = (seq[s] == seq[e]);
+	else if (seq[s] != seq[e]) return 0;
+	else return ret = dp(s + 1, e - 1);
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	memset(cache, -1, sizeof(cache));
+	int n;	cin >> n;
+	for (int i = 1; i < n + 1; i++) cin >> seq[i];
+	int m;	cin >> m;
+	while (m--) {
+		int s, e;	cin >> s >> e;
+		cout << dp(s, e) << '\n';
+	}
+}
