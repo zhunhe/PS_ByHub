138490d664b80d76ccc7b4d405ffd3992db190af
Junhyuk Hur <xuzhunhe@gmail.com>
2023-08-11 07:18:10 +0900

    [Gold V] Title: 비밀번호 만들기, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.cc
+++ 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.cc
@@ -2,27 +2,22 @@
 using namespace std;
 #define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-int dp[42][42];
-string dp_string[42][42];
+string dp[42][42];
 
 int main() {
     FASTIO
     string s1, s2; cin >> s1 >> s2;
+    s1 = " " + s1; s2 = " " + s2;
     const int len1 = s1.size(), len2 = s2.size();
-    for (int y = 1; y < len1 + 1; y++) {
-        for (int x = 1; x < len2 + 1; x++) {
-            if (s1[y - 1] == s2[x - 1]) {
-                dp[y][x] = dp[y - 1][x - 1] + 1;
-                dp_string[y][x] = dp_string[y - 1][x - 1] + s2[x - 1];
-            }
-            else if (dp[y - 1][x] > dp[y][x - 1]) {
+    for (int y = 1; y < len1; y++) {
+        for (int x = 1; x < len2; x++) {
+            if (s1[y] == s2[x])
+                dp[y][x] = dp[y - 1][x - 1] + s2[x];
+            else if (dp[y - 1][x].size() > dp[y][x - 1].size())
                 dp[y][x] = dp[y - 1][x];
-                dp_string[y][x] = dp_string[y - 1][x];
-            } else {
+            else
                 dp[y][x] = dp[y][x - 1];
-                dp_string[y][x] = dp_string[y][x - 1];
-            }
         }
     }
-    cout << dp_string[len1][len2];
+    cout << dp[len1 - 1][len2 - 1] << '\n';
 }
