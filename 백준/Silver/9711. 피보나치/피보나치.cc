a2fae3bf772afb6cc4f286570fffd8c3eeb7dc4a
Junhyuk Hur <xuzhunhe@gmail.com>
2023-08-11 10:28:06 +0900

    [Silver III] Title: 피보나치, Time: 540 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/9711. 피보나치/피보나치.cc
+++ 백준/Silver/9711. 피보나치/피보나치.cc
@@ -2,15 +2,16 @@
 using namespace std;
 #define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
+uint64_t fibo[10002];
+
 int main() {
     FASTIO
     int T; cin >> T;
     for (int i = 1; i < T + 1; i++) {
         int P, Q; cin >> P >> Q;
-        vector<uint64_t> fibo(P + 1);
-        fibo[1] = fibo[2] = 1;
+        fibo[1] = fibo[2] = 1 % Q;
         for (int j = 3; j < P + 1; j++)
             fibo[j] = (fibo[j - 1] + fibo[j - 2]) % Q;
-        cout << "Case #" << i << ": " << fibo[P] % Q << '\n';
+        cout << "Case #" << i << ": " << fibo[P] << '\n';
     }
 }
