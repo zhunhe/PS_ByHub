beca8ab8d40ffe513c8752fe4382a4ed3fe9f0a7
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-06 15:40:19 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.cpp
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.cpp
@@ -6,8 +6,9 @@ using namespace std;
 int solution(int n) {
     int answer = 0;
 
-    for (int i = 1; i < n + 1; i++)
-        if (n / i * i == n)
-            answer++;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) answer += 2;
+        if (i * i == n) answer--;
+    }
     return answer;
 }
