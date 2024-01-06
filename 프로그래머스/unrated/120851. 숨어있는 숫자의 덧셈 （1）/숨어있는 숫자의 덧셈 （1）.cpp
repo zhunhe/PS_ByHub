dbd2b6ca0333366b00b1d6bddcf12a5f1cfbd926
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-06 15:05:59 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+
+    for (char c: my_string)
+        if (isdigit(c))
+            answer += (c - '1' + 1);
+    return answer;
+}
