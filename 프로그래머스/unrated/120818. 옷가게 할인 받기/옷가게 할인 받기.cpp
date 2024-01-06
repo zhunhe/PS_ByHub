e60124a66e5df9e837edb72b19bd6ed96bdc30a5
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-06 14:45:44 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    if (price >= 500000)
+        return price * 0.8;
+    if (price >= 300000)
+        return price * 0.9;
+    if (price >= 100000)
+        return price * 0.95;
+    return price;
+}
