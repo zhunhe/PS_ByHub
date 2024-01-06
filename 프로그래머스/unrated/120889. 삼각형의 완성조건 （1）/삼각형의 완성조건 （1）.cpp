7a5a35726206fec397e7532c9b5007dce88b1cf9
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-07 06:13:49 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> sides) {
+    sort(sides.begin(), sides.end());
+    return sides[2] < sides[0] + sides[1] ? 1 : 2;
+}
