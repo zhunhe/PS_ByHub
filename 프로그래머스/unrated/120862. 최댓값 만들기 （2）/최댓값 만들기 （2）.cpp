016609946e83a903109d4021f5a9e83e90cb4b80
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-08 22:28:05 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -8,8 +8,5 @@ using namespace std;
 int solution(vector<int> numbers) {
     sort(numbers.begin(), numbers.end());
     const int last = numbers.size() - 1;
-    int answer = numbers[0] * numbers[1];
-    answer = max(answer, numbers[0] * numbers[last]);
-    answer = max(answer, numbers[last - 1] * numbers[last]);
-    return answer;
+    return max(numbers[0] * numbers[1], numbers[last - 1] * numbers[last]);
 }
