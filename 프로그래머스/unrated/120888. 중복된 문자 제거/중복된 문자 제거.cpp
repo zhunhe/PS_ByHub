a4d664ab2c8db798b0858f24d0e962a3821ab619
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-08 23:30:10 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.cpp
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -1,16 +1,11 @@
 #include <string>
-#include <set>
 
 using namespace std;
 
 string solution(string my_string) {
     string result = "";
-    set<char> check;
-
-    for (const char c: my_string) {
-        if (check.find(c) != check.end()) continue;
-        result += c;
-        check.insert(c);
-    }
+    for (const char c: my_string)
+        if (result.find(c) == string::npos)
+            result += c;
     return result;
 }
