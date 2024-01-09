0d0e1d7aec275e973b5f3dbb9a378867f128b5eb
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-10 00:16:48 +0900

    [level unrated] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.cpp
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 int solution(vector<int> common) {
-    bool diff = (common[1] - common[0]) == (common[2] - common[1]);
     const int last = common.size() - 1;
-    if (diff)
+    if ((common[1] - common[0]) == (common[2] - common[1]))
         return common[last] + common[last] - common[last - 1];
     return common[last] * common[last] / common[last - 1];
 }
