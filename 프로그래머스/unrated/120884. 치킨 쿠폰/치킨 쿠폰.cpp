a3e48eab2618833db40f35d29c841de9923e777d
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-09 19:13:35 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.cpp
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int solution(int chicken) {
     int answer = 0;
-    while (chicken > 10) {
+    while (chicken >= 10) {
         answer += chicken / 10;
         chicken = (chicken / 10) + (chicken % 10);
     }
-    return answer + chicken / 10;
+    return answer;
 }
