54a109240276b61e64ccb29afcece926be4935d3
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-13 01:27:45 +0900

    [level Lv.0] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    answer.push_back(n);
+    while (n > 1) {
+        if (n & 1)
+            n = 3 * n + 1;
+        else
+            n >>= 1;
+        answer.push_back(n);
+    }
+    return answer;
+}
