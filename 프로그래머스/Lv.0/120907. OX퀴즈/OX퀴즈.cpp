13d2f4c8b7e896b4964c3fab692e3c534689a64b
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-12 19:57:51 +0900

    [level Lv.0] Title: OX퀴즈, Time: 0.02 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120907. OX퀴즈/OX퀴즈.cpp
+++ 프로그래머스/Lv.0/120907. OX퀴즈/OX퀴즈.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool test(stringstream& ss) {
+    int lhs, rhs, result;
+    char oper, tmp;
+    ss >> lhs >> oper >> rhs >> tmp >> result;
+    if (oper == '+')
+        return lhs + rhs == result;
+    return lhs - rhs == result;
+}
+
 vector<string> solution(vector<string> quiz) {
     vector<string> answer;
 
     for (const string& s: quiz) {
         stringstream ss(s);
-        int lhs, rhs, result;
-        char oper, tmp;
-        ss >> lhs >> oper >> rhs >> tmp >> result;
-        if (oper == '+') {
-            if (lhs + rhs == result)
-                answer.push_back("O");
-            else
-                answer.push_back("X");
-        } else if (oper == '-') {
-            if (lhs - rhs == result)
-                answer.push_back("O");
-            else
-                answer.push_back("X");
-        }       
+        answer.push_back(test(ss) ? "O" : "X");
     }
     return answer;
 }
