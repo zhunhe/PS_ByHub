2cf01ef772859d668044cb7f976dd7117dc4bde8
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-13 02:29:58 +0900

    [level Lv.0] Title: 세로 읽기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181904. 세로 읽기/세로 읽기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+using namespace std;
+
+string solution(string s, int m, int c) {
+    string ans = "";
+    for (int i = c - 1; i < s.size(); i += m)
+        ans += s[i];
+    return ans;
+}
