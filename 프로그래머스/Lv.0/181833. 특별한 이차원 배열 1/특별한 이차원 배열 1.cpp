2e7dedee76c3b53334a69aa3a30d64dfd659c29e
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-13 01:54:31 +0900

    [level Lv.0] Title: 특별한 이차원 배열 1, Time: 1.29 ms, Memory: 4.81 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        answer[i][i] = 1;
+    return answer;
+}
