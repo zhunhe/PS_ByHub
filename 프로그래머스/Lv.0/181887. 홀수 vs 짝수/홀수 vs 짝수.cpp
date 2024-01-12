ee521d81ea90acf23a8cb4d0ed6f77d723009b9b
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-13 01:32:58 +0900

    [level Lv.0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
+++ 프로그래머스/Lv.0/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
@@ -4,10 +4,7 @@ using namespace std;
 
 int solution(vector<int> nums) {
     int oddSum = 0, evenSum = 0;
-    if (nums.size() & 1) oddSum = nums.back();
-    for (int i = 1; i < nums.size(); i += 2) {
-        evenSum += nums[i];
-        oddSum += nums[i - 1];
-    }
+    for (int i = 0; i < nums.size(); i++)
+        i & 1 ? evenSum += nums[i] : oddSum += nums[i];
     return max(oddSum, evenSum);
 }
