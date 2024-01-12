a9d9d518edd534b72fe874edd9deb0a2aac392ec
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-13 02:08:49 +0900

    [level Lv.0] Title: 문자열 돌리기, Time: 2.70 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str; cin >> str;
+    for (const char c : str)
+        cout << c << '\n';
+    return 0;
+}
