23eaf069586688c571f6c05a38bac05fe7209787
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-12 23:13:55 +0900

    [level Lv.0] Title: 정수 찾기, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181840. 정수 찾기/정수 찾기.cpp
+++ 프로그래머스/Lv.0/181840. 정수 찾기/정수 찾기.cpp
@@ -4,5 +4,5 @@
 using namespace std;
 
 int solution(vector<int> num_list, int n) {
-    return find(num_list.begin(), num_list.end(), n) == num_list.end() ? 0 : 1;
+    return find(num_list.begin(), num_list.end(), n) != num_list.end();
 }
