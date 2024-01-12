5977f87fc78ee79d81164106770a06814c7b4bcb
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-12 23:32:27 +0900

    [level Lv.0] Title: 원하는 문자열 찾기, Time: 0.29 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
+++ 프로그래머스/Lv.0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
@@ -4,11 +4,7 @@
 using namespace std;
 
 int solution(string myString, string pat) {
-    if (pat.size() > myString.size()) return 0;
     transform(myString.begin(), myString.end(), myString.begin(), ::toupper);
     transform(pat.begin(), pat.end(), pat.begin(), ::toupper);
-    for (int i = 0; i < myString.size() - pat.size() + 1; i++)
-        if (myString.substr(i, pat.size()) == pat)
-            return 1;
-    return 0;
+    return myString.find(pat) != string::npos;
 }
