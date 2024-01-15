db86f68533369dd717031e2141aa8ff20607abc1
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-16 05:48:57 +0900

    [level Lv.1] Title: 크기가 작은 부분문자열, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+int solution(string t, string p) {
+    int answer = 0;
+    for (int i = 0; i < t.size() - p.size() + 1; i++)
+        if (t.substr(i, p.size()) <= p)
+            ++answer;
+    return answer;
+}
