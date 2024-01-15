d32a0e79f537bc291c9af432f609fc27549ee6e4
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-16 04:56:37 +0900

    [level Lv.1] Title: 제일 작은 수 제거하기, Time: 0.15 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ 프로그래머스/Lv.1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
-    int _min = *min_element(arr.begin(), arr.end());
-    arr.erase(remove(arr.begin(), arr.end(), _min), arr.end());
-    if (arr.empty())
-        arr.push_back(-1);
-    return arr;
+    arr.erase(min_element(arr.begin(), arr.end()));
+    return arr.empty() ? vector<int>{-1} : arr;
 }
