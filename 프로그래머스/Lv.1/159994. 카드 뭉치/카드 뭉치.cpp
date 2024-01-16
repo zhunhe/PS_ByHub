5816b36616c7fa9ce659a7c48fee54a5ce1534cf
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-16 17:24:55 +0900

    [level Lv.1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/Lv.1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/Lv.1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -6,9 +6,9 @@ using namespace std;
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
     int idx1 = 0, idx2 = 0;
     for (const string& s : goal) {
-        if (s == cards1[idx1])
+        if (idx1 < cards1.size() && s == cards1[idx1])
             ++idx1;
-        else if (s == cards2[idx2])
+        else if (idx2 < cards2.size() && s == cards2[idx2])
             ++idx2;
         else
             return "No";
