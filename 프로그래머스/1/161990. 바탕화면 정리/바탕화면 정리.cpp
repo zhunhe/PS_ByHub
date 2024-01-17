23c38c43996be4b0c016054ae43a9571f0c48b3b
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-17 12:58:19 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -4,16 +4,17 @@
 using namespace std;
 
 vector<int> solution(vector<string> wallpaper) {
-    int luy = wallpaper.size(), lux = wallpaper[0].size();
-    int rdy = 0, rdx = 0;
+    vector<int> ans = {50, 50, 0, 0};
+
     for (int y = 0; y < wallpaper.size(); y++) {
         for (int x = 0; x < wallpaper[y].size(); x++) {
             if (wallpaper[y][x] == '.') continue;
-            luy = min(luy, y);
-            lux = min(lux, x);
-            rdy = max(rdy, y);
-            rdx = max(rdx, x);
+            ans[0] = min(ans[0], y);
+            ans[1] = min(ans[1], x);
+            ans[2] = max(ans[2], y);
+            ans[3] = max(ans[3], x);
         }
     }
-    return {luy, lux, rdy + 1, rdx + 1};
+    ++ans[2], ++ans[3];
+    return ans;
 }
