3df470abd4cdbf6c53e6749ec1c787f4f96f8e66
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-18 16:28:35 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.55 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -9,8 +9,7 @@ int countOne(int num) {
 
 int solution(int n) {
     int cnt = countOne(n);
-    while (true)
-        if (countOne(++n) == cnt)
-            return n;
-    return 0;
+    while (countOne(++n) != cnt)
+        ;
+    return n;
 }
