461af48e0ed1a0bcd00a22a6ed4059c9932c0734
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-18 16:59:58 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -5,8 +5,8 @@ using namespace std;
 vector<int> solution(int brown, int yellow) {
     int big = brown + yellow;
     int small = yellow;
-    for (int i = 1; i < brown + 1; i++)
-        for (int j = 1; j < brown + 1; j++)
+    for (int i = 1; i < brown / 2; i++)
+        for (int j = 1; j < brown / 2; j++)
             if (i * j == big && (i - 2) * (j - 2) == small)
                 return {max(i, j), min(i, j)};
     return {};
