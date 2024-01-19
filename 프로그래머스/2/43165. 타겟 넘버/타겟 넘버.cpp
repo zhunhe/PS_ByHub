daae89a91ddfdbeb3a9e61aadf7dd2079854be61
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-19 16:37:53 +0900

    [level 2] Title: 타겟 넘버, Time: 0.14 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 
 int solution(vector<int> nums, int target) {
-    int ans = 0;
-    int size = 1 << nums.size();
+    int ans = 0, size = 1 << nums.size();
 
-    for (int i = 1; i < size; i++) {
-        int tmp = 0;
-        for (int j = 0 ; j < nums.size() ; j++)
-            i & (1 << j) ? tmp -= nums[j] : tmp += nums[j];
-        if (tmp == target) ++ans;
+    for (int i = 0; i < size; i++) {
+        int sum = 0;
+        for (int j = 0; j < nums.size(); j++)
+            i & (1 << j) ? sum -= nums[j] : sum += nums[j];
+        if (sum == target) ++ans;
     }
     return ans;
 }
