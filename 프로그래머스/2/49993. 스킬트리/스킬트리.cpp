75ade7e2f6dccbad83c668e527bc0c2ce9c3d3ae
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-19 23:38:01 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+    int ans = 0;
+    for (auto s : skill_trees) {
+        int last = -1; bool ok = true;
+        for (auto c : skill) {
+            int idx = find(s.begin(), s.end(), c) - s.begin();
+            if (last != -1 && idx < last) {
+                ok = false;
+                break;
+            }
+            last = idx;
+        }
+        if (ok) ++ans;
+    }
+    return ans;
+}
