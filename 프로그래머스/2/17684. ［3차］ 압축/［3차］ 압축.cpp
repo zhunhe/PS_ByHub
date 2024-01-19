864d67f74fa18ef9f334e0a4935d86f8f1c7f9d6
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-19 15:56:05 +0900

    [level 2] Title: [3차] 압축, Time: 0.24 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -2,22 +2,15 @@
 using namespace std;
 
 vector<int> solution(string s) {
-    unordered_map<string, int> m;
-    for (int i = 0; i < 26; i++) m[string(1, static_cast<char>('A' + i))] = i + 1;
-
+    map<string, int> m;
+    for (int i = 0; i < 26; i++) m[string(1, 'A' + i)] = i + 1;
     vector<int> ans;
-    const int sLen = s.size();
-    int i = 0, mLen = 2, newIdx = 27;
-    while (i < sLen) {
-        for (int len = mLen; i + len <= sLen || len > 0; len--) {
-            const string subS = s.substr(i, len), c = string(1, s[i + len]);
-            if (!m[subS]) continue;
-            ans.push_back(m[subS]);
-            m[subS + c] = newIdx++;
-            mLen = max(mLen, len + 1);
-            i += len;
-            break;
-        }
+    for (int i = 0; i < s.size();) {
+        string subS = "";
+        while (i < s.size() && m.find(subS + s[i]) != m.end())
+            subS += s[i++];
+        ans.push_back(m[subS]);
+        if (i < s.size()) m[subS + s[i]] = m.size() + 1;
     }
     return ans;
 }
