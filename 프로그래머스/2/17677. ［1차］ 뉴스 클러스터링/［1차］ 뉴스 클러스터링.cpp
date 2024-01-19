77fbe07677bcfbb706cc6e59967fb22bf503a6fb
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-19 18:11:10 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,23 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a, b, arr1[676], arr2[676];
 int solution(string s1, string s2) {
-    transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
-    transform(s2.begin(), s2.end(), s2.begin(), ::toupper);
-    map<string, int> m1, m2;
-    for (int i = 0; i < s1.size() - 1; i++)
-        if (isupper(s1[i]) && isupper(s1[i + 1]))
-            ++m1[s1.substr(i, 2)];
-    for (int i = 0; i < s2.size() - 1; i++)
-        if (isupper(s2[i]) && isupper(s2[i + 1]))
-            ++m2[s2.substr(i, 2)];
-    int inter = 0;
-    for (auto& [key, value] : m1)
-        if (m2.find(key) != m2.end())
-            inter += min(value, m2[key]);
-    int _union = 0;
-    for (auto& [key, value] : m1) _union += value;
-    for (auto& [key, value] : m2) _union += value;
-    _union -= inter;
-    return _union ? inter * 65536 / _union : 65536;
+    for (int i = 1; i < s1.size(); i++)
+        if (isalpha(s1[i - 1]) && isalpha(s1[i]))
+            ++arr1[(s1[i - 1] & 31) * 26 + (s1[i] & 31)];
+    for (int i = 1; i < s2.size(); i++)
+        if (isalpha(s2[i - 1]) && isalpha(s2[i]))
+            ++arr2[(s2[i - 1] & 31) * 26 + (s2[i] & 31)];
+    for (int i = 0; i < 676; i++) a += min(arr1[i], arr2[i]), b += max(arr1[i], arr2[i]);
+    return b ? a * 65536 / b : 65536;
 }
