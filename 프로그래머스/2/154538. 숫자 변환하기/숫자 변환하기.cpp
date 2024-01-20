e4a161c67843770d4cca0a023c9e7b563d8e2aa8
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-20 20:55:26 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.73 ms, Memory: 4.41 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -4,22 +4,18 @@ using namespace std;
 bool visited[1000001];
 
 int solution(int x, int y, int n) {
-    queue<int> q;
+    queue<pair<int, int>> q;
     visited[x] = true;
-    q.push(x);
-    int ans = 0;
+    q.push({x, 0});
     while (!q.empty()) {
-        const int len = q.size();
-        for (int i = 0; i < len; i++) {
-            const int now = q.front(); q.pop();
-            if (now == y) return ans;
-            for (int next : {now + n, now * 2, now * 3}) {
-                if (next > y || visited[next]) continue;
-                visited[next] = true;
-                q.push(next);
-            }
-        }      
-        ++ans;
+        const auto now = q.front(); q.pop();
+        if (now.first == y)
+            return now.second;
+        for (int next : {now.first + n, now.first * 2, now.first * 3}) {
+            if (next > y || visited[next]) continue;
+            visited[next] = true;
+            q.push({next, now.second + 1});
+        }
     }
     return -1;
 }
