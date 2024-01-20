cab64e9e1551739e9855580bb2369323d913be09
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-20 20:00:50 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 29.14 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -2,20 +2,11 @@
 using namespace std;
 
 vector<long long> solution(vector<long long> nums) {
-    const int len = nums.size();
     vector<long long> ans;
     for (auto num : nums) {
-        if (!(num & 1)) {
-            ans.push_back(num + 1);
-            continue;
-        }
-        for (long long i = 1; i < LLONG_MAX; i <<= 1) {
-            if (num & i) continue;
-            num |= i;
-            num ^= (i >> 1);
-            ans.push_back(num);
-            break;
-        }
+        long long i = 1;
+        while (num & i) i <<= 1;
+        ans.push_back(num + i - (i >> 1));
     }
     return ans;
 }
