30bba4c2de6a6d8e401b5537234466533d814ba6
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-22 06:38:10 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 3.80 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -1,21 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int bsq[1002][1002];
-int b[1002][1002];
-
-int solution(vector<vector<int>> board) {
-    for (int i = 0; i < board.size(); i++)
-        for (int j = 0; j < board[0].size(); j++)
-            b[i + 1][j + 1] = board[i][j];
-
-    const int h = board.size() + 1, w = board[0].size() + 1;
-    int ans = 0;
-    for (int i = 1; i < h; i++) {
-        for (int j = 1; j < w; j++) {
+int solution(vector<vector<int>> b) {
+    int ans = b[0][0];
+    for (int i = 1; i < b.size(); i++) {
+        for (int j = 1; j < b[0].size(); j++) {
             if (b[i][j] == 0) continue;
-            bsq[i][j] = min({bsq[i-1][j-1], bsq[i-1][j], bsq[i][j-1]}) + 1;
-            ans = max(ans, bsq[i][j] * bsq[i][j]);
+            b[i][j] = min({b[i-1][j-1], b[i-1][j], b[i][j-1]}) + 1;
+            ans = max(ans, b[i][j] * b[i][j]);
         }
     }
     return ans;
