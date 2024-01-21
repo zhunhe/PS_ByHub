13632527b1139757837cbe2e63b892304e112322
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-22 03:26:04 +0900

    [level 2] Title: 호텔 대실, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -1,26 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define all(x)  (x).begin(), (x).end()
-
 int conv(string t) {
     const int h = (t[0] - '0') * 10 + (t[1] - '0');
     const int m = (t[3] - '0') * 10 + (t[4] - '0');
     return h * 60 + m;
 }
 
-bool compare(const vector<string>& lhs, const vector<string>& rhs) {
-    if (lhs[0] == rhs[0])
-        return lhs[1] < rhs[1];
-    return lhs[0] < rhs[0];
-}
-
 int solution(vector<vector<string>> book_time) {
-    sort(all(book_time), compare);
+    sort(book_time.begin(), book_time.end());
     priority_queue<int> pq;
     for (auto time : book_time) {
-        const int s = conv(time[0]);
-        const int e = conv(time[1]);
+        const int s = conv(time[0]), e = conv(time[1]);
         if (!pq.empty() && -pq.top() <= s)
             pq.pop();
         pq.push(-(e + 10));
