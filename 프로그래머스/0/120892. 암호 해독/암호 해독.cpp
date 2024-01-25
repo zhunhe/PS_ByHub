e6b3e4aa79c0ce56d2e058504af691fef882e5ab
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-25 19:38:09 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string solution(string cipher, int code) {
+    string ans = "";
+    for (int i = code - 1; i < cipher.size(); i += code)
+        ans += cipher[i];
+    return ans;
+}
