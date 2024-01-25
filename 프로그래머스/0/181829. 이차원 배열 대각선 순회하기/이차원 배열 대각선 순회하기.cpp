f34e0c249972e2deb9fd6a6bc6ef3c153590ec8f
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-25 22:16:41 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<vector<int>> board, int k) {
+    int ans = 0;
+    for (int i = 0; i < board.size(); i++)
+        for (int j = 0; j < board[0].size(); j++)
+            if (i + j <= k)
+                ans += board[i][j];
+    return ans;
+}
