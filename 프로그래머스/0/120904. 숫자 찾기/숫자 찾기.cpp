7d9f22b438ff96247e08d3cb8a8c0abc8ede1255
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-25 21:46:03 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int solution(int num, int k) {
-    int idx = to_string(num).find(k + '0');
-    return idx == -1 ? -1 : idx + 1;
+    int pos = to_string(num).find(k + '0');
+    return pos == string::npos ? -1 : pos + 1;
 }
