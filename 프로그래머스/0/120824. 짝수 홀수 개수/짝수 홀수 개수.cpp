544d91ae97991007f2323b22db8384bc910cb29d
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-25 14:18:50 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -4,6 +4,6 @@ using namespace std;
 vector<int> solution(vector<int> num_list) {
     vector<int> ans(2);
     for (auto num : num_list)
-        num & 1 ? ++ans[1] : ++ans[0];
+        ++ans[!!(num & 1)];
     return ans;
 }
