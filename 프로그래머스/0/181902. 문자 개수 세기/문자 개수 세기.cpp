c5fb2e739bd60f688fdf4749e12ba674d718f38f
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-26 15:51:12 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 vector<int> solution(string s) {
     vector<int> ans(52);
-    for (const auto& c : s) ++ans[c - (c & 32 ? 71 : 65)];
+    for (const auto& c : s) ++ans[c - (c & 32 ? 'a' - 26 : 'A')];
     return ans;
 }
