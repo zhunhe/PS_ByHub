a5c34fb97404a1b8a1128c3e0edfad28ee33463d
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-26 14:52:27 +0900

    [level 0] Title: qr code, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string solution(int q, int r, string code) {
+    string ans = "";
+    for (int i = r; i < code.size(); i += q)
+        ans += code[i];
+    return ans;
+}
