5f0b314945a85cfc2049189af105756aca554682
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-26 17:02:08 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 300.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
@@ -1,12 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int comb(int n, int r) {
+int solution(int n, int r) {
     if (n == r || r == 0)
         return 1;
-    return comb(n - 1, r - 1) + comb(n - 1, r);
-}
-
-int solution(int balls, int share) {
-    return comb(balls, share);
+    return solution(n - 1, r - 1) + solution(n - 1, r);
 }
