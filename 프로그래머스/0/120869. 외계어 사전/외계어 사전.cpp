d8da89d784664c25164f1dca4435f559ba478210
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-26 17:57:56 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.cpp
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int solution(vector<string> v, vector<string> dic) {
     sort(v.begin(), v.end());
-    const string spell = accumulate(v.begin(), v.end(), string(""));
+    const string spell = reduce(v.cbegin(), v.cend(), string(""));
     for (auto& s : dic) sort(s.begin(), s.end());
     for (const auto& s : dic) if (s == spell) return 1;
     return 2;
