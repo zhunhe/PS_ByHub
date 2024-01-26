06f9bfab6c4ff456d2b28ef4b621cfdb339bbbee
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-26 23:23:47 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int a, int b, int c, int d) {
+    set<int> st = {a, b, c, d};
+    switch (st.size()) {
+        case 1: return 1111 * a;
+        case 2:
+            if (a == b && c == d) return (a + c) * abs(a - c);
+            if (a == c && b == d) return (a + b) * abs(a - b);
+            if (a == d && b == c) return (a + b) * abs(a - b);
+
+            if (b == c && c == d) return pow(10 * b + a, 2);
+            if (a == c && c == d) return pow(10 * c + b, 2);
+            if (a == b && b == d) return pow(10 * d + c, 2);
+            if (a == b && b == c) return pow(10 * a + d, 2);
+        case 3:
+            if (a == b) return c * d;
+            if (a == c) return b * d;
+            if (a == d) return b * c;
+            if (b == c) return a * d;
+            if (b == d) return a * c;
+            if (c == d) return a * b;
+        case 4: return min({a, b, c, d});
+    }
+    return 0;
+}
