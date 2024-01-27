7f8332507195928c1f694d79673a235659aa0445
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-27 15:55:45 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -3,5 +3,5 @@ using namespace std;
 
 bool solution(int x) {
     string s = to_string(x);
-    return !(x % accumulate(s.begin(), s.end(), -'0' * s.size()));
+    return !(x % reduce(s.begin(), s.end(), -'0' * s.size()));
 }
