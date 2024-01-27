f25f15c3ca513286afeb753ed3e66ac4e45056d9
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-27 14:31:18 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.cpp
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.cpp
@@ -2,11 +2,5 @@
 using namespace std;
 
 int solution(string A, string B) {
-    if (A == B) return 0;
-    for (int i = 0; i < B.size(); i++) {
-        rotate(A.begin(), A.end() - 1, A.end());
-        if (A == B)
-            return i + 1;
-    }
-    return -1;
+    return (B + B).find(A);
 }
