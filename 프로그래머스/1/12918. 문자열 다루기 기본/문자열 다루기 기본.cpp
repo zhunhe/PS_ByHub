7ea734b9dd761686b01b7a823c8f30499a43f5f7
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-27 19:07:03 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solution(string s) {
+    if (s.size() != 4 && s.size() != 6) return false;
+    for (auto c : s) if (!isdigit(c)) return false;
+    return true;
+}
