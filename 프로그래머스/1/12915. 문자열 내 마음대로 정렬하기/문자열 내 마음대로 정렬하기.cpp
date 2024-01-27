9a25122f66a0625f700f572534721df7a08f4ba5
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-27 21:35:44 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.06 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 vector<string> solution(vector<string> strings, int n) {
     sort(strings.begin(), strings.end(), [=](auto lhs, auto rhs) {
-        if (lhs[n] == rhs[n])
-            return lhs < rhs;
-        return lhs[n] < rhs[n];
+        return lhs[n] == rhs[n] ? lhs < rhs : lhs[n] < rhs[n];
     });
     return strings;
 }
