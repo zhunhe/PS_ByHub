041f3c784bc32feadd1fa9d21a0ea2b68f1fa2bf
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-27 21:55:42 +0900

    [level 1] Title: 추억 점수, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    unordered_map<string, int> m;
+    for (int i = 0; i < name.size(); i++) m[name[i]] = yearning[i];
+    vector<int> ans;
+    for (auto v : photo) {
+        const int point = accumulate(v.cbegin(), v.cend(), 0, [&](auto sum, auto s) {
+            return sum + (m.find(s) != m.end() ? m[s] : 0);
+        });
+        ans.push_back(point);
+    }
+    return ans;
+}
