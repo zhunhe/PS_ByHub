006346c4bad54adb65291c516faeb1391b094ea5
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-27 23:11:45 +0900

    [level 1] Title: 덧칠하기, Time: 0.41 ms, Memory: 4.93 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -1,13 +1,16 @@
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int solution(int n, int m, vector<int> section) {
-    int ans = 1, last = section[0];
-    for (int point : section) {
-        if (point >= last + m) {
-            last = point;
-            ++ans;
+    vector<bool> painted(n + 1, true);
+    for (auto n : section) painted[n] = false;
+    int ans = 0;
+    for (int i = 1; i < n + 1; i++) {
+        if (painted[i]) continue;
+        ++ans;
+        for (int j = 0; j < m; j++) {
+            if (i + j == n + 1) break;
+            painted[i + j] = true;
         }
     }
     return ans;
