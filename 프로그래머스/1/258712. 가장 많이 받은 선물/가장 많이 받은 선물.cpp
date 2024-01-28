e8aaec4508ea1d34b7468b1883287cc40011073c
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 22:11:37 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1.37 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -5,17 +5,15 @@ int solution(vector<string> friends, vector<string> gifts) {
     unordered_map<string, int> indexByName;
     const int len = friends.size();
     for (int i = 0; i < len; i++) indexByName[friends[i]] = i;
+    vector<int> points(len);
     vector<vector<int>> record(len, vector<int>(len));
     for (const auto& s : gifts) {
         stringstream ss(s);
         string from, to; ss >> from >> to;
         ++record[indexByName[from]][indexByName[to]];
+        ++points[indexByName[from]];
+        --points[indexByName[to]];
     }
-    vector<int> points(len);
-    for (int i = 0; i < len; i++) points[i] = reduce(record[i].begin(), record[i].end());
-    for (int j = 0; j < len; j++)
-        for (int i = 0; i < len; i++)
-            points[j] -= record[i][j];
     vector<int> presents(len);
     for (int from = 0; from < len; from++) {
         for (int to = from + 1; to < len; to++) {
