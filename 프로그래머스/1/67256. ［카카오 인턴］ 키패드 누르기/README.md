a019cf5496dfc4f75cf58de39ceee8f61b6680a1
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 17:33:41 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.08 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.10 ms
+메모리: 4.15 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 12:10:26
+2024년 1월 0일 17:33:39
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
@@ -1,40 +1,40 @@
-#include <string>
-#include <vector>
-#include <map>
-
-#include<iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-const map<char, pair<int, int>> m = {
-    {'1', {0,0}}, {'2', {0,1}}, {'3', {0,2}},
-    {'4', {1,0}}, {'5', {1,1}}, {'6', {1,2}},
-    {'7', {2,0}}, {'8', {2,1}}, {'9', {2,2}},
-    {'*', {3,0}}, {'0', {3,1}}, {'#', {3,2}}
+unordered_map<char, pair<int, int>> pos = {
+    {'1', {0, 0}}, {'2', {0, 1}}, {'3', {0, 2}},
+    {'4', {1, 0}}, {'5', {1, 1}}, {'6', {1, 2}},
+    {'7', {2, 0}}, {'8', {2, 1}}, {'9', {2, 2}},
+    {'*', {3, 0}}, {'0', {3, 1}}, {'#', {3, 2}}
 };
 
-int diff(char now, char next) {
-    return abs(m.at(now).first - m.at(next).first) + abs(m.at(now).second - m.at(next).second);
+int distance(char now, char next) {
+    auto [y1, x1] = pos[now];
+    auto [y2, x2] = pos[next];
+    return abs(y1 - y2) + abs(x1 - x2);
 }
 
 string solution(vector<int> numbers, string hand) {
-    char l = '*', r = '#';
-    string answer = "";
-    for (int num : numbers) {
-        const int diffL = diff(l, num + '0');
-        const int diffR = diff(r, num + '0');
-        if (num % 3 == 1)
-            answer += "L", l = num + '0';
-        else if (num != 0 && num % 3 == 0)
-            answer += "R", r = num + '0';
-        else if (diffL < diffR)
-            answer += "L", l = num + '0';
-        else if (diffR < diffL)
-            answer += "R", r = num + '0';
-        else if (hand == "right")
-            answer += "R", r = num + '0';
-        else
-            answer += "L", l = num + '0';
+    string ans = "";
+    char left = '*', right = '#';
+    for (auto now : numbers) {
+        switch (now) {
+            case 1: case 4: case 7: ans += "L"; left = now + '0'; break;
+            case 3: case 6: case 9: ans += "R"; right = now + '0'; break;
+            case 2: case 5: case 8: case 0:
+                const int distLeft = distance(left, now + '0');
+                const int distRight = distance(right, now + '0');
+                if (distLeft < distRight) {
+                    ans += "L"; left = now + '0';
+                } else if (distLeft > distRight) {
+                    ans += "R"; right = now + '0';
+                } else if (hand == "left") {
+                    ans += "L"; left = now + '0';
+                } else {
+                    ans += "R"; right = now + '0';
+                }
+                break;
+        }
     }
-    return answer;
+    return ans;
 }
