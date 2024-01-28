3aa6368b7c6e3d10591501b884b54e7df33fdfd9
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 16:52:40 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061#) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.03 ms
+메모리: 4.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 11:43:18
+2024년 1월 0일 16:52:38
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -1,23 +1,22 @@
-#include <string>
-#include <vector>
-#include <stack>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
-    stack<int> moved;
-    int answer = 0;
-    for (int picked : moves) {
-        for (int i = 0; i < board.size(); i++) {
-            if (board[i][picked - 1] == 0) continue;
-            if (!moved.empty() && moved.top() == board[i][picked - 1]) {
-                answer += 2;
-                moved.pop();
-            } else
-                moved.push(board[i][picked - 1]);
-            board[i][picked - 1] = 0;
+    int ans = 0;
+    stack<int> st;
+    for (auto move : moves) {
+        const int x = move - 1;
+        for (int y = 0; y < board.size(); y++) {
+            if (!board[y][x]) continue;
+            if (st.empty() || board[y][x] != st.top()) {
+                st.push(board[y][x]);
+            } else {
+                st.pop();
+                ans += 2;
+            }
+            board[y][x] = 0;
             break;
         }
     }
-    return answer;
+    return ans;
 }
