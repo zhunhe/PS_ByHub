07b255c5506ab1c050c85cfeb50c58925eaf8662
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 16:31:16 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
@@ -1,19 +1,15 @@
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 const int dy[4] = {-1, 0, 1, 0};
 const int dx[4] = {0, 1, 0, -1};
 
-int solution(vector<vector<string>> b, int y, int x) {
+int solution(vector<vector<string>> board, int y, int x) {
     int ans = 0;
     for (int k = 0; k < 4; k++) {
-        const int ny = y + dy[k];
-        const int nx = x + dx[k];
-        if (ny < 0 || nx < 0 || ny >= b.size() || nx >= b[0].size()) continue;
-        if (b[ny][nx] != b[y][x]) continue;
-        ++ans;
+        const int ny = y + dy[k], nx = x + dx[k];
+        if (ny < 0 || nx < 0 || ny >= board.size() || nx >= board[0].size()) continue;
+        ans += board[y][x] == board[ny][nx];
     }
     return ans;
 }
