6d2281e9338de25200a8c548ea7ba9c292ec70fd
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 15:41:51 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -1,21 +1,13 @@
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 string solution(string X, string Y) {
-    vector<int> cntX(10), cntY(10);
-    for (char c : X) ++cntX[c - '0'];
-    for (char c : Y) ++cntY[c - '0'];
-    string answer = "";
-    for (int i = 9; i >= 0; i--) {
-        int cnt = min(cntX[i], cntY[i]);
-        while (cnt--)
-            answer += (i + '0');
-    }
-    if (answer.size() > 1 && answer[0] == '0')
-        answer = "0";
-    if (answer.empty())
-        answer += "-1";
-    return answer;
+    vector<int> cnt1(10), cnt2(10);
+    for (auto c : X) ++cnt1[c - '0'];
+    for (auto c : Y) ++cnt2[c - '0'];
+    string ans = "";
+    for (int i = 9; i > 0; i--) ans += string(min(cnt1[i], cnt2[i]), i + '0');
+    if (min(cnt1[0], cnt2[0])) ans += ans.empty() ? "0" : string(min(cnt1[0], cnt2[0]), '0');
+    if (ans.empty()) ans = "-1";
+    return ans;
 }
