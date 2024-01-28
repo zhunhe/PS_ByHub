14898b8b83cb621604ffb30700b71de0d8241ccd
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 22:09:23 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1.29 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,38 +1,33 @@
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <sstream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int solution(vector<string> friends, vector<string> gifts) {
     unordered_map<string, int> indexByName;
-    unordered_map<int, string> nameByIndex;
     const int len = friends.size();
-    for (int i = 0; i < len; i++) {
-        indexByName[friends[i]] = i;
-        nameByIndex[i] = friends[i];
-    }
+    for (int i = 0; i < len; i++) indexByName[friends[i]] = i;
     vector<vector<int>> record(len, vector<int>(len));
-    unordered_map<string, int> futureByName;
-    for (const string& gift : gifts) {
-        stringstream ss(gift);
+    for (const auto& s : gifts) {
+        stringstream ss(s);
         string from, to; ss >> from >> to;
         ++record[indexByName[from]][indexByName[to]];
-        ++futureByName[from];
-        --futureByName[to];
     }
-    int answer = 0;
-    for (int i = 0; i < len; i++) {
-        int cnt = 0;
-        for (int j = 0; j < len; j++) {
-            if (record[i][j] > record[j][i])
-                ++cnt;
-            else if (record[i][j] == record[j][i])
-                if (futureByName[nameByIndex[i]] > futureByName[nameByIndex[j]])
-                    ++cnt;
+    vector<int> points(len);
+    for (int i = 0; i < len; i++) points[i] = reduce(record[i].begin(), record[i].end());
+    for (int j = 0; j < len; j++)
+        for (int i = 0; i < len; i++)
+            points[j] -= record[i][j];
+    vector<int> presents(len);
+    for (int from = 0; from < len; from++) {
+        for (int to = from + 1; to < len; to++) {
+            if (record[from][to] > record[to][from])
+                ++presents[from];
+            else if (record[from][to] < record[to][from])
+                ++presents[to];
+            else if (points[from] > points[to])
+                ++presents[from];
+            else if (points[from] < points[to])
+                ++presents[to];
         }
-        answer = max(answer, cnt);
     }
-    return answer;
+    return *max_element(presents.begin(), presents.end());
 }
