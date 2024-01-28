9a5f1f1343464448023d39b92db2ead979022bac
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 21:43:33 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.cpp
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int solution(vector<int> bandage, int health, vector<vector<int>> attacks) {
-    int t = 0;
+    int before = 0;
     const int maxHealth = health;
     for (const auto& v : attacks) {
-        const int diff = v[0] - t - 1;
+        const int now = v[0], damage = v[1], diff = now - before - 1;
         health += diff * bandage[1] + diff / bandage[0] * bandage[2];
         health = min(health, maxHealth);
-        health -= v[1];
+        health -= damage;
         if (health <= 0) return -1;
-        t = v[0];
+        before = now;
     }
     return health;
 }
