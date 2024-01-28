d200c249fd17a3029b1e3fdde0e59bd14fe3eab5
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 21:16:18 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.31 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -1,48 +1,35 @@
-#include <string>
-#include <vector>
-#include <regex>
-#include <unordered_map>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int nowYear, nowMonth, nowDay;
-unordered_map<char, int> termByType;
-
-void initToday(string today) {
+int conv(string today) {
     today = regex_replace(today, regex("\\."), " ");
     stringstream ss(today);
-    ss >> nowYear >> nowMonth >> nowDay;
+    string year, month, day;
+    ss >> year >> month >> day;
+    return stoi(year) * 28 * 12 + stoi(month) * 28 + stoi(day);
 }
 
-void initTerms(const vector<string>& terms) {
-    for (const string& s : terms) {
+unordered_map<char, int> makeTerm(const vector<string>& terms) {
+    unordered_map<char, int> term;
+    for (const auto& s : terms) {
         stringstream ss(s);
-        char type; int term;
-        ss >> type >> term;
-        termByType[type] = term;
+        char type; int period;
+        ss >> type >> period;
+        term[type] = period;
     }
+    return term;
 }
 
 vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
-    initToday(today);
-    initTerms(terms);
-    vector<int> answer;
+    const int nToday = conv(today);
+    unordered_map<char, int> term = makeTerm(terms);
+    vector<int> ans;
     for (int i = 0; i < privacies.size(); i++) {
         stringstream ss(privacies[i]);
         string date; char type;
         ss >> date >> type;
-
-        date = regex_replace(date, regex("\\."), " ");
-        ss.str(date);
-        int year, month, day;
-        ss >> year >> month >> day;
-        month += termByType[type];
-        while (month > 12)
-            ++year, month -= 12;
-        if (year > nowYear) continue;
-        if (year == nowYear && month > nowMonth) continue;
-        if (year == nowYear && month == nowMonth && day > nowDay) continue;
-        answer.push_back(i + 1);
+        const int expiration = conv(date) + term[type] * 28;
+        if (expiration <= nToday) ans.push_back(i + 1);
     }
-    return answer;
+    return ans;
 }
