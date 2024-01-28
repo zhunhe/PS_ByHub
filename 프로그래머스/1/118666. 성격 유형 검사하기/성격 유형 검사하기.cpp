b98ab12ad4c1d1ac711945b1f6c4cc56e08a1512
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 19:38:54 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
@@ -1,23 +1,24 @@
-#include <string>
-#include <vector>
-#include <map>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 string solution(vector<string> survey, vector<int> choices) {
-    // RCJA
-    // TFMN
-    map<char, int> m;
+    int r = 0, t = 0, c = 0, f = 0, j = 0, m = 0, a = 0, n = 0;
     for (int i = 0; i < survey.size(); i++) {
-        if (choices[i] > 4)
-            m[survey[i][1]] += choices[i] - 4;
-        else
-            m[survey[i][0]] += 4 - choices[i];
+        switch (survey[i][0]) {
+            case 'R': choices[i] > 4 ? t += choices[i] - 4 : r += 4 - choices[i]; break;
+            case 'T': choices[i] > 4 ? r += choices[i] - 4 : t += 4 - choices[i]; break;
+            case 'C': choices[i] > 4 ? f += choices[i] - 4 : c += 4 - choices[i]; break;
+            case 'F': choices[i] > 4 ? c += choices[i] - 4 : f += 4 - choices[i]; break;
+            case 'J': choices[i] > 4 ? m += choices[i] - 4 : j += 4 - choices[i]; break;
+            case 'M': choices[i] > 4 ? j += choices[i] - 4 : m += 4 - choices[i]; break;
+            case 'A': choices[i] > 4 ? n += choices[i] - 4 : a += 4 - choices[i]; break;
+            case 'N': choices[i] > 4 ? a += choices[i] - 4 : n += 4 - choices[i]; break;
+        }
     }
-    string answer = "";
-    answer += m['R'] >= m['T'] ? "R" : "T";
-    answer += m['C'] >= m['F'] ? "C" : "F";
-    answer += m['J'] >= m['M'] ? "J" : "M";
-    answer += m['A'] >= m['N'] ? "A" : "N";
-    return answer;
+    string ans = "";
+    ans += r >= t ? "R" : "T";
+    ans += c >= f ? "C" : "F";
+    ans += j >= m ? "J" : "M";
+    ans += a >= n ? "A" : "N";
+    return ans;
 }
