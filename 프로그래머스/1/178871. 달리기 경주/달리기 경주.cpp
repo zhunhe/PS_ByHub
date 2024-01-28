f587b2e556706a278580f7277ca7ce6b3a3cd83d
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-28 20:53:01 +0900

    [level 1] Title: 달리기 경주, Time: 0.02 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
@@ -1,18 +1,24 @@
-#include <string>
-#include <vector>
-#include <unordered_map>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 vector<string> solution(vector<string> players, vector<string> callings) {
-    unordered_map<string, int> m;
-    for (int i = 0; i < players.size(); i++) m[players[i]] = i;
-    for (const string& calling : callings) {
-        if (m[calling] == 0) continue;
-        int idx = m[calling];
-        swap(players[idx - 1], players[idx]);
-        m[players[idx - 1]] = idx - 1;
-        m[players[idx]] = idx;
+    unordered_map<string, int> indexByName;
+    unordered_map<int, string> nameByIndex;
+    for (int i = 0; i < players.size(); i++) {
+        indexByName[players[i]] = i;
+        nameByIndex[i] = players[i];
+    }
+    for (const auto& call : callings) {
+        int idx = indexByName[call];
+        if (idx > 0) {
+            swap(players[idx - 1], players[idx]);
+
+            string behind = players[idx];
+            indexByName[call] = idx - 1;
+            indexByName[behind] = idx;
+            nameByIndex[idx - 1] = call;
+            nameByIndex[idx] = behind;
+        }
     }
     return players;
 }
