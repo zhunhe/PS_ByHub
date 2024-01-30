c53cf36152b8209fa93cd15f9e806cd256e97ee6
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-30 18:25:11 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     map<string, int> m;
-    for (auto& v : clothes) ++m[v[1]];
+    for (auto v : clothes) ++m[v[1]];
     int ans = 1;
-    for (auto& [k, v] : m) ans *= (v + 1);
+    for (auto [key, value] : m) ans *= (value + 1);
     return ans - 1;
 }
