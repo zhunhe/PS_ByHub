14c1a722620f6a049a732d1394571aa358627758
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-30 23:21:35 +0900

    [level 2] Title: 땅따먹기, Time: 6.04 ms, Memory: 25.7 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -2,15 +2,11 @@
 using namespace std;
 
 int solution(vector<vector<int>> land) {
-    const int h = land.size(), w = land[0].size();
-    vector<vector<int>> dp(h, vector<int>(w));
-    dp[0] = land[0];
-    for (int y = 1; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            for (int i = 0; i < w; i++) if (i != x)
-                dp[y][x] = max(dp[y][x], dp[y - 1][i]);
-            dp[y][x] += land[y][x];
-        }
+    for (int i = 1; i < land.size(); i++) {
+        land[i][0] += max({land[i - 1][1], land[i - 1][2], land[i - 1][3]});
+        land[i][1] += max({land[i - 1][0], land[i - 1][2], land[i - 1][3]});
+        land[i][2] += max({land[i - 1][0], land[i - 1][1], land[i - 1][3]});
+        land[i][3] += max({land[i - 1][0], land[i - 1][1], land[i - 1][2]});
     }
-    return *max_element(dp[h-1].begin(), dp[h-1].end());
+    return *max_element(land[land.size() - 1].begin(), land[land.size() - 1].end());
 }
