a8997883c89cf24c3bf666ea5822f75534df93e2
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-30 18:06:05 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 3.65 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -1,10 +1,12 @@
-#include <cmath>
+#include <bits/stdc++.h>
+using namespace std;
 
 int solution(int n, int a, int b) {
-    int ans = 0;
-    while (a != b) {
-        a = (a + 1) >> 1;
-        b = (b + 1) >> 1;
+    int ans = 1;
+    --a, --b;
+    while (a > 0 || b > 0) {
+        a >>= 1, b >>= 1;
+        if (a == b) return ans;
         ++ans;
     }
     return ans;
