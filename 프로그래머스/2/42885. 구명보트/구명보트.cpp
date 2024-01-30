28aedc3a6a6724ee49ef4354b4f0623975a57eff
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-30 16:55:46 +0900

    [level 2] Title: 구명보트, Time: 1.19 ms, Memory: 4.63 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -1,15 +1,14 @@
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int solution(vector<int> people, int limit) {
-    sort(people.begin(), people.end());
     int ans = 0, l = 0, r = people.size() - 1;
+    sort(people.begin(), people.end());
     while (l <= r) {
-        ++ans;
         if (people[l] + people[r] <= limit)
-            ++l, --r;
+            ++l, --r, ++ans;
+        else if (people[r] <= limit)
+            --r, ++ans;
         else
             --r;
     }
