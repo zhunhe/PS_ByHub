cdfef081187b85e0cb20ad17e3d42ef03efe49ba
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-30 22:58:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.15 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int dy[4] = {-1, 0, 1, 0};
@@ -7,16 +7,17 @@ const int dx[4] = {0, 1, 0, -1};
 int solution(vector<vector<int>> maps) {
     const int h = maps.size(), w = maps[0].size();
     queue<pair<int, int>> q;
-    q.push({0, 0}); maps[0][0] = 1;
+    q.push({0, 0});
+    maps[0][0] = 2;
     while (!q.empty()) {
-        const int y = q.front().first, x = q.front().second; q.pop();
+        const auto [y, x] = q.front(); q.pop();
         for (int k = 0; k < 4; k++) {
             const int ny = y + dy[k], nx = x + dx[k];
             if (ny < 0 || nx < 0 || ny >= h || nx >= w) continue;
             if (maps[ny][nx] != 1) continue;
-            maps[ny][nx] = maps[y][x] + 1;
             q.push({ny, nx});
+            maps[ny][nx] = maps[y][x] + 1;
         }
     }
-    return maps[h - 1][w - 1] <= 1 ? -1 : maps[h - 1][w - 1];
-} 
+    return maps[h - 1][w - 1] == 1 ? -1 : maps[h - 1][w - 1] - 1;
+}
