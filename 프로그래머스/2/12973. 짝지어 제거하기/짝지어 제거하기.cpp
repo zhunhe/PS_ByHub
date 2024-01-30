6f68ef516980db113222c121975e1575e4c5a5d4
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-30 14:16:51 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.21 ms, Memory: 6.79 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -1,12 +1,12 @@
-#include <string>
-#include <stack>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int solution(string s)
-{
-    stack<char> st;
-    for (char c : s)
-        (st.empty() || st.top() != c) ? st.push(c) : st.pop();
+int solution(string s) {
+    vector<char> st;
+    for (const auto& c : s) {
+        st.push_back(c);
+        while (st.size() > 1 && st[st.size() - 2] == st[st.size() - 1])
+            st.pop_back(), st.pop_back();
+    }
     return st.empty();
 }
