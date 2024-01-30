e8c0055a4d756827c86a07960677a9394a0a43a2
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-30 23:04:20 +0900

    [level 2] Title: 더 맵게, Time: 216.05 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
-    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
+    priority_queue<int> pq;
+    for (auto elem : scoville) pq.push(-elem);
     int ans = 0;
-    while (pq.size() > 1 && pq.top() < K) {
-        int a = pq.top(); pq.pop();
-        int b = pq.top(); pq.pop();
-        pq.push(a + b * 2);
+    while (pq.size() > 1 && -pq.top() < K) {
+        const int a = pq.top(); pq.pop();
+        const int b = pq.top() * 2; pq.pop();
+        pq.push(a + b);
         ++ans;
     }
-    return pq.empty() || pq.top() < K ? -1 : ans;
+    return -pq.top() < K ? -1 : ans;
 }
