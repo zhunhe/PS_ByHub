f0ca4333c1cf9edb3e32ae50fe6d66d06d3f2bfb
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-30 20:03:43 +0900

    [level 2] Title: 전화번호 목록, Time: 78.77 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool solution(vector<string> phone_book) {
-    unordered_map<string, int> m;
-    for (auto& hp : phone_book)
-        for (int i = 1; i < hp.size(); i++)
-            m[hp.substr(0, i)] = hp.size();
-    for (auto& hp : phone_book)
-        if (m.find(hp) != m.end() && m[hp] != hp.size())
+bool solution(vector<string> v) {
+    sort(v.begin(), v.end());
+    for (int i = 1; i < v.size(); i++) {
+        const int len = min(v[i - 1].size(), v[i].size());
+        if (v[i - 1].substr(0, len) == v[i].substr(0, len))
             return false;
+    }
     return true;
 }
