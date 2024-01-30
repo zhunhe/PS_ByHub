8960d4ba6e3d1fde79cd6f35a32a55d28f0d2636
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-30 17:50:01 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -1,15 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solution(vector<string> w, vector<int> n, vector<string> d) {
-    unordered_map<string, int> hash;
-    for (int i = 0; i < w.size(); i++) hash[w[i]] = n[i];
-    for (int i = 0; i < 9; i++) if ((--hash[d[i]]) == 0) hash.erase(d[i]);
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    map<string, int> m;
+    for (int i = 0; i < want.size(); i++) m[want[i]] = number[i];
+    for (int i = 0; i < 9; i++)
+        if (--m[discount[i]] == 0)
+            m.erase(discount[i]);
     int ans = 0;
-    for (int i = 9; i < d.size(); i++) {
-        if ((--hash[d[i]]) == 0) hash.erase(d[i]);
-        if (hash.empty()) ++ans;
-        if ((++hash[d[i - 9]]) == 0) hash.erase(d[i - 9]);
+    for (int i = 9; i < discount.size(); i++) {
+        if (--m[discount[i]] == 0)
+            m.erase(discount[i]);
+        if (m.empty())
+            ++ans;
+        if (++m[discount[i - 9]] == 0)
+            m.erase(discount[i - 9]);
     }
     return ans;
 }
