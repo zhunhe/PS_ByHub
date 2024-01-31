cd1e10fb7fcfe9390b7af74d51409bffd66fba40
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-31 18:17:35 +0900

    [level 2] Title: 오픈채팅방, Time: 141.25 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -2,23 +2,27 @@
 using namespace std;
 
 vector<string> solution(vector<string> record) {
-    unordered_map<string, string> nickByUID;
+    unordered_map<string, string> m;    // uid, nick
     vector<string> ans;
-    for (auto s : record) {
+    for (const auto& s : record) {
         stringstream ss(s);
         string cmd, uid, nick;
         ss >> cmd >> uid;
         if (cmd == "Enter" || cmd == "Change") {
             ss >> nick;
-            nickByUID[uid] = nick;
+            m[uid] = nick;
         }
         if (cmd == "Enter" || cmd == "Leave")
             ans.push_back(uid + " " + cmd);
     }
     for (auto& s : ans) {
         stringstream ss(s);
-        string uid, cmd; ss >> uid >> cmd;
-        s = nickByUID[uid] + "님이 " + (cmd == "Enter" ? "들어왔습니다." : "나갔습니다.");
+        string uid, cmd;
+        ss >> uid >> cmd;
+        if (cmd == "Enter")
+            s = m[uid] + "님이 들어왔습니다.";
+        else
+            s = m[uid] + "님이 나갔습니다.";
     }
     return ans;
 }
