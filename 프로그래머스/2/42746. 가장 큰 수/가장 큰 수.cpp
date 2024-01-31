6db850d24d27f3ebad5ca6f8f2f1498d677e45e6
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-31 13:00:03 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -1,16 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool cmp(const string& lhs, const string& rhs) {
-    return lhs + rhs < rhs + lhs;
-}
-
-string solution(vector<int> nums) {
-    vector<string> v;
-    for (int num : nums) v.push_back(to_string(num));
-    sort(v.rbegin(), v.rend(), cmp);
-    string ans = "";
-    for (string s : v) ans += s;
-    while (ans.size() > 1 && ans[0] == '0') ans = ans.substr(1);
-    return ans;
+string solution(vector<int> v) {
+    deque<string> ans;
+    for (const auto& elem : v) ans.push_back(to_string(elem));
+    sort(ans.begin(), ans.end(), [](auto lhs, auto rhs) { return lhs + rhs > rhs + lhs; });
+    while (ans.size() > 1 && ans.front() == "0") ans.pop_front();
+    return accumulate(ans.begin(), ans.end(), string(""));
 }
