8eae0b28b39d2a0718ac7246ead8ae6e1556d003
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-31 13:03:10 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -4,7 +4,7 @@ using namespace std;
 string solution(vector<int> v) {
     vector<string> ans;
     for (const auto& elem : v) ans.push_back(to_string(elem));
-    sort(ans.begin(), ans.end(), [](auto lhs, auto rhs) { return lhs + rhs > rhs + lhs; });
+    sort(ans.begin(), ans.end(), [](const auto& lhs, const auto& rhs) { return lhs + rhs > rhs + lhs; });
     if (ans[0] == "0") return "0";
     return accumulate(ans.begin(), ans.end(), string(""));
 }
