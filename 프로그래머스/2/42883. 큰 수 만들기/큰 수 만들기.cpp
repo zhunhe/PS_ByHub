833ab5c9e39f2ff5f5dc59acde14a169d8709823
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-31 13:24:42 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 string solution(string number, int k) {
     string ans = "";
-    for (char c : number) {
-        while (!ans.empty() && k && ans.back() < c)
+    for (const auto& c : number) {
+        while (!ans.empty() && k > 0 && ans.back() < c)
             ans.pop_back(), --k;
-        ans += c;
+        ans.push_back(c);
     }
-    while (k--) ans.pop_back();
+    while (k > 0) ans.pop_back(), --k;
     return ans;
 }
