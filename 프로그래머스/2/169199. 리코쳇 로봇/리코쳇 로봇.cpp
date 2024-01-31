ae849fae560187816e948ab0ecbf563f8c818af6
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-31 17:45:07 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.04 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int visited[101][101];
-
 const int dy[4] = {-1, 0, 1, 0};
 const int dx[4] = {0, 1, 0, -1};
 
-pair<int, int> calculateNext(const vector<string>& board, int y, int x, int dir) {
+pair<int, int> calNextCoor(const vector<string>& board, int y, int x, int dir) {
     while (1) {
         const int ny = y + dy[dir], nx = x + dx[dir];
         if (ny < 0 || nx < 0 || ny >= board.size() || nx >= board[0].size()) break;
@@ -17,24 +15,22 @@ pair<int, int> calculateNext(const vector<string>& board, int y, int x, int dir)
 }
 
 int solution(vector<string> board) {
-    const int h = board.size(), w = board[0].size();
+    vector<vector<int>> visited(board.size(), vector<int>(board[0].size()));
     queue<pair<int, int>> q;
-    int ans = 0;
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[0].size(); j++) {
             if (board[i][j] != 'R') continue;
-            q.push({i, j});
             visited[i][j] = 1;
+            q.push({i, j});
             break;
         }
     }
-    
     while (!q.empty()) {
-        auto [y, x] = q.front(); q.pop();
+        const auto [y, x] = q.front(); q.pop();
         if (board[y][x] == 'G')
             return visited[y][x] - 1;
-        for (int dir = 0; dir < 4; dir++) {
-            auto [ny, nx] = calculateNext(board, y, x, dir);
+        for (int k = 0; k < 4; k++) {
+            const auto [ny, nx] = calNextCoor(board, y, x, k);
             if (visited[ny][nx]) continue;
             visited[ny][nx] = visited[y][x] + 1;
             q.push({ny, nx});
