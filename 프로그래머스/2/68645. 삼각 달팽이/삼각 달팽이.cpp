ea942aa1f7d64a6c8a7ab7e23e2a6ec23f98aea3
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-31 13:18:02 +0900

    [level 2] Title: 삼각 달팽이, Time: 163.72 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
@@ -4,26 +4,19 @@ using namespace std;
 const int dy[3] = {1, 0, -1};
 const int dx[3] = {0, 1, -1};
 
-int b[1001][1001], y, x, dir;
-
-bool isNextBlocked(int n) {
-    const int ny = y + dy[dir], nx = x + dx[dir];
-    return ny < 0 || ny >= n || nx < 0 || nx >= n || b[ny][nx];
-}
-
 vector<int> solution(int n) {
-    const int len = n * (n + 1) / 2;
-    for (int i = 1; i <= len; i++) {
+    vector<vector<int>> b(n, vector<int>(n));
+    int y = 0, x = 0, dir = 0;
+    const int sum = n * (n + 1) >> 1;
+    for (int i = 1; i < sum + 1; i++) {
         b[y][x] = i;
-        if (isNextBlocked(n))
-            dir = (dir + 1) % 3;
+        const int ny = y + dy[dir], nx = x + dx[dir];
+        if (ny >= n || nx >= n || b[ny][nx]) dir = (dir + 1) % 3;
         y += dy[dir], x += dx[dir];
     }
-
     vector<int> ans;
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            if (b[i][j])
-                ans.push_back(b[i][j]);
+        for (int j = 0; j < i + 1; j++)
+            ans.push_back(b[i][j]);
     return ans;
 }
