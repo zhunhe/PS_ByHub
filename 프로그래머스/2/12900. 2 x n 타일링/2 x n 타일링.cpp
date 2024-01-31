ced25d08e3984fa65be5aae448c8255d9b0ec856
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-31 12:26:46 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.26 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MOD = 1e9 + 7;
+
 int solution(int n) {
     vector<int> dp(n + 1);
-    for (int i = 1; i < 4; i++) dp[i] = i;
-    for (int i = 4; i < n + 1; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+    dp[1] = 1, dp[2] = 2;
+    for (int i = 3; i < n + 1; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
     return dp[n];
 }
