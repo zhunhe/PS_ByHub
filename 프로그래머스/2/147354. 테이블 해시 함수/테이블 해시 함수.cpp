6e41b10b45b22d876331987ef4751bebae2c121b
Junhyuk Hur <xuzhunhe@gmail.com>
2024-01-31 19:19:06 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -1,23 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int idx;
-
-bool compare(const vector<int>& lhs, const vector<int>& rhs) {
-    if (lhs[idx] == rhs[idx])
-        return lhs[0] > rhs[0];
-    return lhs[idx] < rhs[idx];
-}
-
 int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
-    idx = col - 1;
-    sort(data.begin(), data.end(), compare);
+    sort(data.begin(), data.end(), [=](auto lhs, auto rhs) {
+        if (lhs[col - 1] == rhs[col - 1])
+            return lhs[0] > rhs[0];
+        return lhs[col - 1] < rhs[col - 1];
+    });
     int ans = 0;
     for (int i = row_begin - 1; i < row_end; i++) {
-        int modSum = 0;
-        for (int j = 0; j < data[0].size(); j++)
-            modSum += data[i][j] % (i + 1);
-        ans ^= modSum;
+        int sum = 0;
+        for (const auto& num : data[i]) sum += num % (i + 1);
+        ans ^= sum;
     }
     return ans;
 }
