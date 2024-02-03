403e24c927d4277bd65e03df11b484117abf9159
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-03 13:19:07 +0900

    [level 2] Title: 방문 길이, Time: 0.05 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.02 ms
+메모리: 4.17 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 22:40:49
+2024년 2월 6일 13:19:4
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int len = 11;
-
-bool visited[121][121];
-unordered_map<char, int> dir = {{'U', 0}, {'R', 1}, {'D', 2}, {'L', 3}};
-
 const int dy[4] = {-1, 0, 1, 0};
 const int dx[4] = {0, 1, 0, -1};
 
-int solution(string s) {
-    int y = 5, x = 5, ans = 0;
-    for (auto c : s) {
+unordered_map<char, int> dir = { {'U', 0}, {'R', 1}, {'D', 2}, {'L', 3} };
+
+const int len = 11;
+
+int solution(string dirs) {
+    int y = 5, x = 5;
+    vector<vector<int>> visited(121, vector<int>(121));
+    for (const auto c : dirs) {
         const int ny = y + dy[dir[c]], nx = x + dx[dir[c]];
         if (ny < 0 || nx < 0 || ny >= len || nx >= len) continue;
-        if (!visited[y * len + x][ny * len + nx]) ++ans;
-        visited[y * len + x][ny * len + nx] = true;
-        visited[ny * len + nx][y * len + x] = true;
+        visited[ny * len + nx][y * len + x] = 1;
+        visited[y * len + x][ny * len + nx] = 1;
         y = ny, x = nx;
     }
-    return ans;
+    int ans = 0;
+    for (const auto& v : visited)
+        ans += reduce(v.begin(), v.end());
+    return ans >> 1;
 }
