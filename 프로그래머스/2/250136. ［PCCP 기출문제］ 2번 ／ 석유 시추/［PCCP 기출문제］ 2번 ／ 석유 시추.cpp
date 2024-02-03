aa8d43f1fbed062e0779114ea3fa042b915ef81e
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-03 23:13:37 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 1.73 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -4,40 +4,39 @@ using namespace std;
 const int dy[4] = {-1, 0, 1, 0};
 const int dx[4] = {0, 1, 0, -1};
 
-vector<int> amount;
-
-void bfs(vector<vector<int>>& land, int y, int x) {
-    const int h = land.size(), w = land[0].size();
-    int cnt = 0;
-    set<int> pos;
-    queue<pair<int, int>> q;
-    pos.insert(x);
-    q.push({y, x});
-    land[y][x] = 0;
-    while (!q.empty()) {
-        auto [y, x] = q.front(); q.pop();
-        ++cnt;
-        pos.insert(x);
-        for (int k = 0; k < 4; k++) {
-            const int ny = y + dy[k], nx = x + dx[k];
-            if (ny < 0 || nx < 0 || ny >= h || nx >= w) continue;
-            if (land[ny][nx] == 0) continue;
-            land[ny][nx] = 0;
-            q.push({ny, nx});
-        }
-    }
-    for (int idx : pos)
-        amount[idx] += cnt;
-}
-
 int solution(vector<vector<int>> land) {
-    const int h = land.size(), w = land[0].size();
-    amount.resize(w);
-    for (int j = 0; j < w; j++) {
-        for (int i = 0; i < h; i++) {
+    int num = 1;
+    vector<set<int>> cnt(land[0].size());
+    unordered_map<int, int> m;   // number, size
+    for (int i = 0; i < land.size(); i++) {
+        for (int j = 0; j < land[0].size(); j++) {
             if (land[i][j] == 0) continue;
-            bfs(land, i, j);
+            queue<pair<int, int>> q;
+            land[i][j] = 0;
+            q.push({i, j});
+            int size = 1;
+            cnt[j].insert(num);
+            while (!q.empty()) {
+                const auto [y, x] = q.front(); q.pop();
+                for (int k = 0; k < 4; k++) {
+                    const int ny = y + dy[k], nx = x + dx[k];
+                    if (ny < 0 || nx < 0 || ny >= land.size() || nx >= land[0].size()) continue;
+                    if (land[ny][nx] == 0) continue;
+                    ++size;
+                    land[ny][nx] = 0;
+                    cnt[nx].insert(num);
+                    q.push({ny, nx});
+                }
+            }
+            m[num++] = size;
         }
     }
-    return *max_element(amount.begin(), amount.end());
+    int ans = 0;
+    for (int i = 0; i < land[0].size(); i++) {
+        int sum = 0;
+        for (auto elem : cnt[i])
+            sum += m[elem];
+        ans = max(ans, sum);
+    }
+    return ans;
 }
