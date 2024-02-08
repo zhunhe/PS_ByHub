5bbb5c1a457ad060183ee2e01560631f732632fb
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-08 13:27:18 +0900

    [level 2] Title: [3차] 압축, Time: 0.20 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> solution(string s) {
-    map<string, int> m;
-    for (int i = 0; i < 26; i++) m[string(1, 'A' + i)] = i + 1;
+vector<int> solution(string msg) {
     vector<int> ans;
-    for (int i = 0; i < s.size();) {
-        string subS = "";
-        while (i < s.size() && m.find(subS + s[i]) != m.end())
-            subS += s[i++];
-        ans.push_back(m[subS]);
-        if (i < s.size()) m[subS + s[i]] = m.size() + 1;
+    map<string, int> m;
+    for (int i = 0; i < 26; i++) m[string(1, 'A' + i)] = m.size() + 1;
+    for (int pos = 0; pos < msg.size(); pos++) {
+        int len = 2;
+        for (; len < msg.size() - pos + 1; len++)
+            if (m.find(msg.substr(pos, len)) == m.end())
+                break;
+        ans.push_back(m[msg.substr(pos, len - 1)]);
+        m[msg.substr(pos, len)] = m.size() + 1;
+        pos += len - 2;
     }
     return ans;
 }
