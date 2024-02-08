693bdb6e499ba80d0ba0d0e74594462b3481e9bd
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-08 13:29:30 +0900

    [level 2] Title: [3차] 압축, Time: 0.22 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -4,15 +4,17 @@ using namespace std;
 vector<int> solution(string msg) {
     vector<int> ans;
     map<string, int> m;
-    for (int i = 0; i < 26; i++) m[string(1, 'A' + i)] = m.size() + 1;
-    for (int pos = 0; pos < msg.size(); pos++) {
+    for (int i = 0; i < 26; i++)
+        m[string(1, 'A' + i)] = m.size() + 1;
+    int pos = 0;
+    while (pos < msg.size()) {
         int len = 2;
         for (; len < msg.size() - pos + 1; len++)
             if (m.find(msg.substr(pos, len)) == m.end())
                 break;
         ans.push_back(m[msg.substr(pos, len - 1)]);
         m[msg.substr(pos, len)] = m.size() + 1;
-        pos += len - 2;
+        pos += len - 1;
     }
     return ans;
 }
