f3850a83bbada795d7b564e55627416c04eee027
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-08 14:43:44 +0900

    [level 2] Title: 무인도 여행, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.cpp
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.cpp
@@ -6,29 +6,28 @@ const int dx[4] = {0, 1, 0, -1};
 
 vector<int> solution(vector<string> maps) {
     vector<int> ans;
-    vector<vector<bool>> v(maps.size(), vector<bool>(maps[0].size()));
     for (int i = 0; i < maps.size(); i++) {
         for (int j = 0; j < maps[0].size(); j++) {
-            if (maps[i][j] == 'X' || v[i][j]) continue;
+            if (maps[i][j] == 'X') continue;
+            int day = maps[i][j] & 15;
+            maps[i][j] = 'X';
             queue<pair<int, int>> q;
-            v[i][j] = true;
             q.push({i, j});
-            int cnt = (maps[i][j] - '0');
             while (!q.empty()) {
-                auto [y, x] = q.front(); q.pop();
+                const auto [y, x] = q.front(); q.pop();
                 for (int k = 0; k < 4; k++) {
                     const int ny = y + dy[k], nx = x + dx[k];
                     if (ny < 0 || nx < 0 || ny >= maps.size() || nx >= maps[0].size()) continue;
-                    if (maps[ny][nx] == 'X' || v[ny][nx]) continue;
-                    v[ny][nx] = true;
+                    if (maps[ny][nx] == 'X') continue;
+                    day += maps[ny][nx] & 15;
+                    maps[ny][nx] = 'X';
                     q.push({ny, nx});
-                    cnt = cnt + (maps[ny][nx] - '0');
                 }
             }
-            ans.push_back(cnt);
+            ans.push_back(day);
         }
     }
-    if (ans.empty()) ans.push_back(-1);
     sort(ans.begin(), ans.end());
+    if (ans.empty()) ans.push_back(-1);
     return ans;
 }
