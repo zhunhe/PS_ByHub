158d9607b8dba51b57c90bec86c8dc6700cf6d10
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-08 16:21:48 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.12 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.cpp
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.cpp
@@ -4,46 +4,44 @@ using namespace std;
 vector<string> conv(vector<string> v, string oper) {
     vector<string> ret;
     for (int i = 0; i < v.size(); i++) {
-        if (v[i] == oper) {
-            string lhs = ret.back(); ret.pop_back();
-            if (oper == "*")
-                ret.push_back(to_string(stoll(lhs) * stoll(v[i + 1])));
-            else if (oper == "+")
-                ret.push_back(to_string(stoll(lhs) + stoll(v[i + 1])));
-            else if (oper == "-")
-                ret.push_back(to_string(stoll(lhs) - stoll(v[i + 1])));
-            ++i;
-        } else
+        if (v[i] != oper)
             ret.push_back(v[i]);
+        else {
+            long long num;
+            if (oper == "*")
+                num = stoll(ret.back()) * stoll(v[i + 1]);
+            else if (oper == "+")
+                num = stoll(ret.back()) + stoll(v[i + 1]);
+            else if (oper == "-")
+                num = stoll(ret.back()) - stoll(v[i + 1]);
+            ret[ret.size() - 1] = to_string(num);
+            ++i;
+        }
     }
     return ret;
 }
 
-long long solution(string s) {
-    s = regex_replace(s, regex("\\+"), " + ");
-    s = regex_replace(s, regex("\\*"), " * ");
-    s = regex_replace(s, regex("-"), " - ");
-    stringstream ss(s);
-    vector<string> v, ret; string tmpS;
-    while (ss >> tmpS) ret.push_back(tmpS);
+long long solution(string expression) {
+    expression = regex_replace(expression, regex("-"), " - ");
+    expression = regex_replace(expression, regex("\\+"), " + ");
+    expression = regex_replace(expression, regex("\\*"), " * ");
+    stringstream ss(expression);
+    vector<string> v;
+    string s;
+    while (ss >> s) v.push_back(s);
+    // for (auto elem : v) cout << elem << ' ';
     long long ans = 0;
     // * + -
-    v = conv(ret, "*"); v = conv(v, "+"); v = conv(v, "-");
-    ans = max(ans, abs(stoll(v.back())));
+    ans = max(ans, abs(stoll(conv(conv(conv(v, "*"), "+"), "-")[0])));
     // * - +
-    v = conv(ret, "*"); v = conv(v, "-"); v = conv(v, "+");
-    ans = max(ans, abs(stoll(v.back())));
+    ans = max(ans, abs(stoll(conv(conv(conv(v, "*"), "-"), "+")[0])));
     // + * -
-    v = conv(ret, "+"); v = conv(v, "*"); v = conv(v, "-");
-    ans = max(ans, abs(stoll(v.back())));
+    ans = max(ans, abs(stoll(conv(conv(conv(v, "+"), "*"), "-")[0])));
     // + - *
-    v = conv(ret, "+"); v = conv(v, "-"); v = conv(v, "*");
-    ans = max(ans, abs(stoll(v.back())));
+    ans = max(ans, abs(stoll(conv(conv(conv(v, "+"), "-"), "*")[0])));
     // - * +
-    v = conv(ret, "-"); v = conv(v, "*"); v = conv(v, "+");
-    ans = max(ans, abs(stoll(v.back())));
+    ans = max(ans, abs(stoll(conv(conv(conv(v, "-"), "*"), "+")[0])));
     // - + *
-    v = conv(ret, "-"); v = conv(v, "+"); v = conv(v, "*");
-    ans = max(ans, abs(stoll(v.back())));
+    ans = max(ans, abs(stoll(conv(conv(conv(v, "-"), "+"), "*")[0])));
     return ans;
 }
