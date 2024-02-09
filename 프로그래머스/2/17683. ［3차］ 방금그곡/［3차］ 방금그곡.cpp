ff280ed84048f30924b7fd80dc21961f78486f62
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-09 19:07:48 +0900

    [level 2] Title: [3차] 방금그곡, Time: 2.10 ms, Memory: 4.41 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
@@ -1,47 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string replaceSharpToLower(string s) {
-    map<string, string> m = { {"C#", "c"}, {"D#", "d"}, {"F#", "f"}, {"G#", "g"}, {"A#", "a"} };
-    for (auto [key, value] : m) s = regex_replace(s, regex(key), value);
-    return s;
-}
+map<string, string> dict = { {"C#", "c"}, {"D#", "d"}, {"F#", "f"}, {"G#", "g"}, {"A#", "a"} };
 
-int calculateLen(string s, string e) {
-    const int sTime = (s[0] * 10 + s[1]) * 60 + (s[3] * 10 + s[4]);
-    const int eTime = (e[0] * 10 + e[1]) * 60 + (e[3] * 10 + e[4]);
-    return eTime - sTime;
-}
-
-string conv(string s, int len) {
-    string tmp = s;
-    while (s.size() < len) s += tmp;
-    if (s.size() > len) s = s.substr(0, len);
-    return s;
+int conv(const string& t) {
+    const int h = stoi(t.substr(0, 2));
+    const int m = stoi(t.substr(3));
+    return h * 60 + m;
 }
 
 string solution(string m, vector<string> musicinfos) {
-    m = replaceSharpToLower(m);
+    for (const auto& [key, val] : dict)
+        m = regex_replace(m ,regex(key), val);
     string ans = "";
     int maxLen = 0;
-    for (auto musicinfo : musicinfos) {
-        stringstream ss(musicinfo);
-        string s, e, title, repeat;
-
-        getline(ss, s, ',');
-        getline(ss, e, ',');
+    for (auto info : musicinfos) {
+        stringstream ss(info);
+        string start, end, title, song;
+        getline(ss, start, ',');
+        getline(ss, end, ',');
         getline(ss, title, ',');
-        getline(ss, repeat, ',');
-
-        repeat = replaceSharpToLower(repeat);
-        const int len = calculateLen(s, e);
-        repeat = conv(repeat, len);
-
-        if (repeat.find(m) == string::npos) continue;
-        if (maxLen < len) {
-            ans = title;
-            maxLen = len;
-        }
+        getline(ss, song, ',');
+        const int len = conv(end) - conv(start);
+        if (len <= maxLen) continue;
+        for (const auto& [key, val] : dict)
+            song = regex_replace(song ,regex(key), val);
+        while (song.size() < len) song += song;
+        song = song.substr(0, len);
+        if (song.find(m) == string::npos) continue;
+        ans = title;
+        maxLen = len;
     }
     return ans.empty() ? "(None)" : ans;
 }
