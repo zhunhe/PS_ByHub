e4b732889d13dd04f4d450c7d3d0398a745a4d1b
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-11 15:35:42 +0900

    [level 2] Title: 택배상자, Time: 10.67 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.cpp
+++ 프로그래머스/2/131704. 택배상자/택배상자.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int solution(vector<int> order) {
-    int ans = 0; stack<int> st;
+    int ans = 0;
+    stack<int> stk;
     for (int i = 1; i < order.size() + 1; i++) {
-        st.push(i);
-        while (!st.empty() && order[ans] == st.top()) {
-            st.pop();
+        stk.push(i);
+        while (!stk.empty() && stk.top() == order[ans]) {
+            stk.pop();
             ++ans;
         }
     }
