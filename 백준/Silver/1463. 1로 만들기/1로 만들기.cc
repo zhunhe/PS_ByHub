9935f0f54d9ecdc846700fb7723f0e2b53315beb
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-12 11:05:45 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -3,34 +3,12 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-    vector<int> dist(n + 1, INT_MAX);
-    dist[n] = 0;
-    queue<int> q;
-    q.push(n);
-    while (!q.empty()) {
-        const auto now = q.front(); q.pop();
-        if (now <= 1) continue;
-        if (now % 3 == 0) {
-            const int next = now / 3;
-            if (dist[next] > dist[now] + 1) {
-                dist[next] = dist[now] + 1;
-                q.push(next);
-            }
-        }
-        if (!(now & 1)) {
-            const int next = now >> 1;
-            if (dist[next] > dist[now] + 1) {
-                dist[next] = dist[now] + 1;
-                q.push(next);
-            }
-        }
-        if (now > 1) {
-            const int next = now - 1;
-            if (dist[next] > dist[now] + 1) {
-                dist[next] = dist[now] + 1;
-                q.push(next);
-            }
-        }
+    vector<int> dp(n + 1, n);
+    dp[1] = 0;
+    for (int i = 1; i < n; i++) {
+        dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+        if (i * 2 <= n) dp[i * 2] = min(dp[i * 2], dp[i] + 1);
+        if (i * 3 <= n) dp[i * 3] = min(dp[i * 3], dp[i] + 1);
     }
-    cout << dist[1] << '\n';
+    cout << dp[n] << '\n';
 }
