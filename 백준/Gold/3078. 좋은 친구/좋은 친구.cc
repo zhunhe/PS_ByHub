167834e00758d87190315f9167b127d96a26f14c
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-14 03:28:56 +0900

    [Gold IV] Title: 좋은 친구, Time: 244 ms, Memory: 4372 KB -BaekjoonHub

---
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n, k, ans = 0;
+    cin >> n >> k;
+    vector<long long> a(n);
+    map<long long, long long> m;
+    for (auto &i : a) {
+        string s; cin >> s;
+        i = s.length();
+   }
+    int l = 0;
+    for (int i = 0; i < n; i++) {
+        ans += m[a[i]];
+        m[a[i]]++;
+        if (i >= k) m[a[l++]]--;
+    }
+    cout << ans;
+}
