b2e1f45097910b03ff9b5a13485429ea6a524f38
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-14 16:48:02 +0900

    [level 2] Title: 괄호 변환, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.cpp
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.cpp
@@ -5,9 +5,18 @@ bool isRightString(string s) {
     int cnt = 0;
     for (const auto c : s) {
         c == '(' ? ++cnt : --cnt;
-        if (cnt < 0) return false;
+        if (cnt < 0) break;
     }
     return cnt == 0;
+}
+
+pair<string, string> split(string s) {
+    int cnt = 0;
+    for (int i = 0; i < s.size(); i++) {
+        s[i] == '(' ? ++cnt : --cnt;
+        if (cnt == 0)
+            return {s.substr(0, i + 1), s.substr(i + 1)};
+    }
 }
 
 string flip(string s) {
@@ -16,17 +25,10 @@ string flip(string s) {
     return s;
 }
 
-string solution(string s) {
-    if (s == "") return "";
-    int cnt = 0;
-    for (int i = 0; i < s.size(); i++) {
-        s[i] == '(' ? ++cnt : --cnt;
-        if (cnt != 0) continue;
-        string u = s.substr(0, i + 1);
-        string v = s.substr(i + 1);
-        if (isRightString(u))
-            return u + solution(v);
-        return "(" + solution(s.substr(i + 1)) + ")" + flip(u.substr(1, u.size() - 2));
-    }
-    return solution(s);
+string solution(string p) {
+    if (p == "") return "";
+    const auto [u, v] = split(p);
+    return (isRightString(u))
+        ? u + solution(v)
+        : "(" + solution(v) + ")" + flip(u.substr(1, u.size() - 2));
 }
