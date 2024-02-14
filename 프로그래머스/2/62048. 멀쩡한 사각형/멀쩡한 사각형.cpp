69f8e98c609217dcb3f06895b8a0267629cc2e7f
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-14 19:09:09 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -1,20 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt(int y, int x) {
-    if (y > x) swap(y, x);
-    const double lean = (double)y / x;
-    int ret = 1;
-    for (int i = 1; i < x; i++)  {
-        ++ret;
-        const int left = (i - 1) * lean;
-        const int right = i * lean;
-        if (left < right) ++ret;
-    }
-    return ret;
-}
-
 long long solution(int w, int h) {
-    const int _gcd = gcd(w, h);
-    return (long long)w * h - cnt(w / _gcd, h / _gcd) * _gcd;
+    return (long long)w * h - (w + h - gcd(w, h));
 }
