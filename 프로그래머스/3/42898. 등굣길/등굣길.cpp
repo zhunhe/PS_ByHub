aeb7a5abb6a3c26688288ccb2007f10cdfdc1ed2
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-14 21:52:23 +0900

    [level 3] Title: 등굣길, Time: 0.04 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -4,23 +4,16 @@ using namespace std;
 const int MOD = 1e9 + 7;
 
 int solution(int w, int h, vector<vector<int>> puddles) {
-    vector<vector<long long>> dp(h, vector<long long>(w));
-    for (const auto& v : puddles) dp[v[1] - 1][v[0] - 1] = -1;
-    for (int i = 0; i < w; i++) {
-        if (dp[0][i] == -1) break;
-        dp[0][i] = 1;
-    }
-    for (int i = 0; i < h; i++) {
-        if (dp[i][0] == -1) break;
-        dp[i][0] = 1;
-    }
-    for (int i = 1; i < h; i++) {
-        for (int j = 1; j < w; j++) {
-            if (dp[i][j] == -1) continue;
-            if (dp[i - 1][j] != -1) dp[i][j] += dp[i - 1][j] % MOD;
-            if (dp[i][j - 1] != -1) dp[i][j] += dp[i][j - 1] % MOD;
-            dp[i][j] %= MOD;
+    vector<vector<int>> dp(h + 1, vector<int>(w + 1));
+    for (const auto& v : puddles) dp[v[1]][v[0]] = -1;
+    dp[1][1] = 1;
+    for (int i = 1; i <= h; i++) {
+        for (int j = 1; j <= w; j++) {
+            if (dp[i][j] == -1)
+                dp[i][j] = 0;
+            else
+                dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]) % MOD;
         }
     }
-    return dp[h - 1][w - 1];
+    return dp[h][w];
 }
