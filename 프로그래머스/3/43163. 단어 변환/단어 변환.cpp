4374070ff3297e76760c552d3ff796450bfdbc8e
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-14 21:33:40 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,26 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int calDiff(string a, string b) {
-    int diff = 0;
+vector<int> adj[51];
+
+int diff(string a, string b) {
+    int cnt = 0;
     for (int i = 0; i < a.size(); i++)
-        if (a[i] != b[i]) ++diff;
-    return diff;
+        if (a[i] != b[i])
+            ++cnt;
+    return cnt;
 }
 
 int solution(string begin, string target, vector<string> words) {
-    unordered_map<string, bool> visited;
-    queue<pair<string, int>> q;
-    q.push({begin, 0});
+    unordered_map<string, int> m;
+    m[begin] = 0;
+    for (const auto& s : words) {
+        if (m.find(s) == m.end())
+            m[s] = m.size();
+    }
+    for (int i = 0; i < words.size(); i++) {
+        if (diff(begin, words[i]) != 1) continue;
+        adj[0].push_back(m[words[i]]);
+        adj[m[words[i]]].push_back(0);
+    }
+    for (int i = 0; i < words.size(); i++) {
+        for (int j = i + 1; j < words.size(); j++) {
+            if (diff(words[i], words[j]) != 1) continue;
+            adj[m[words[i]]].push_back(m[words[j]]);
+            adj[m[words[j]]].push_back(m[words[i]]);
+        }
+    }
+    vector<int> dist(m.size());
+    dist[m[begin]] = 1;
+    queue<int> q;
+    q.push(m[begin]);
     while (!q.empty()) {
-        const auto [now, cnt] = q.front(); q.pop();
-        if (now == target)
-            return cnt;
-        for (const auto& word : words) {
-            if (calDiff(word, now) != 1) continue;
-            if (visited.find(word) != visited.end()) continue;
-            visited[word] = true;
-            q.push({word, cnt + 1});
+        const auto now = q.front(); q.pop();
+        if (now == m[target])
+            return dist[m[target]] - 1;
+        for (const auto next : adj[now]) {
+            if (dist[next]) continue;
+            dist[next] = dist[now] + 1;
+            q.push(next);
         }
     }
     return 0;
