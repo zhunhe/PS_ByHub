882f350a687c49f6d5bb2bd9fedf02df6ff0d834
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-14 19:47:20 +0900

    [level 2] Title: 점 찍기, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 long long solution(int k, int d) {
-    long long ans = 0;
-    for (int i = 0; i < d + 1; i += k) {
-        ans += (long long)sqrt(powl(d, 2) - powl(i, 2)) / k + 1;
+    long long ans = 0, dd = (long long)d * d;
+    for (long long i = 0; i < d + 1; i += k) {
+        ans += (int)sqrt(dd - i * i) / k + 1;
     }
     return ans;
 }
