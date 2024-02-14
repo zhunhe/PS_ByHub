dcf54600fc145d3d80f3e7d6a31181b1e77a1088
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-14 21:11:42 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> adj[201];
+int visited[201][201];
+
 int solution(int n, vector<vector<int>> computers) {
-    vector<int> adj[201];
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j || computers[i][j] == 0) continue;
+        for (int j = i + 1; j < n; j++) {
+            if (computers[i][j] == 0) continue;
             adj[i].push_back(j);
             adj[j].push_back(i);
         }
     }
     int ans = 0;
-    vector<bool> visited(n + 1);
+    vector<int> visited(n);
     for (int i = 0; i < n; i++) {
-        if (visited[i]) continue;
-        ++ans;
-        queue<int> q;
-        q.push(i);
-        visited[i] = true;
-        while (!q.empty()) {
-            const int now = q.front(); q.pop();
-            for (const int next : adj[now]) {
-                if (visited[next]) continue;
-                visited[next] = true;
-                q.push(next);
+        for (int j = 0; j < n; j++) {
+            if (computers[i][j] == 0 || visited[i]) continue;
+            ++ans;
+            visited[i] = 1;
+            queue<int> q;
+            q.push(i);
+            while (!q.empty()) {
+                const auto now = q.front(); q.pop();
+                for (const auto next : adj[now]) {
+                    if (visited[next]) continue;
+                    visited[next] = 1;
+                    q.push(next);
+                }
             }
         }
     }
