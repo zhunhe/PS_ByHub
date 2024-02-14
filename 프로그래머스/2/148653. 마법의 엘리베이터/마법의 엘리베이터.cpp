1482fb6e6a21dda2c9a3e407f6008aefbd09727e
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-14 16:33:18 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -4,13 +4,19 @@ using namespace std;
 int solution(int storey) {
     int ans = 0;
     while (storey > 0) {
-        const int digit = storey % 10;
+        const int units = storey % 10;
+        if (units == 5) {
+            ans += 5;
+            const int tens = storey / 10 % 10;
+            if (tens >= 5)
+                storey += 5;
+        } else if (units > 5) {
+            ans += 10 - units;
+            storey += 10 - units;
+        }
+        else
+            ans += units;
         storey /= 10;
-        if (digit > 5 || (digit == 5 && storey % 10 >= 5)) {
-            ans += (10 - digit);
-            ++storey;
-        } else
-            ans += digit;
     }
     return ans;
 }
