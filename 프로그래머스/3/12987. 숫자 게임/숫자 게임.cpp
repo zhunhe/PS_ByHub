909e1783b6bb0efcc29a9cae935fce1035180abb
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-15 22:09:45 +0900

    [level 3] Title: 숫자 게임, Time: 15.26 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int solution(vector<int> A, vector<int> B) {
     sort(A.rbegin(), A.rend());
-    sort(B.rbegin(), B.rend());
-    for (int idx = 0, i = 0; i < A.size(); i++) {
-        if (A[i] >= B[idx]) {
-            A[i] = 0;
-        } else
-            ++idx;
+    priority_queue<int> pq;
+    for (const auto b : B) pq.push(b);
+    int ans = 0;
+    for (const auto a : A) {
+        if (pq.top() > a) {
+            pq.pop();
+            ++ans;
+        }
     }
-    return A.size() - count(A.begin(), A.end(), 0);
+    return ans;
 }
