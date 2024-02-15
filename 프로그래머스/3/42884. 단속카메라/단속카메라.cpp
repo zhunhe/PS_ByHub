1fde6eeabffbb1d0314ab3225ee9ef935d40941b
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-15 22:13:20 +0900

    [level 3] Title: 단속카메라, Time: 3.72 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int solution(vector<vector<int>> routes) {
     sort(routes.begin(), routes.end(), [](auto lhs, auto rhs) {
         if (lhs[1] == rhs[1])
-            return lhs[0] < rhs[0];
+            return lhs[0] < lhs[0];
         return lhs[1] < rhs[1];
     });
-    int ans = 0, end = -300000;
-    for (auto& v : routes) {
-        if (v[0] > end) {
+    int ans = 0, end = -30001;
+    for (const auto& v : routes) {
+        if (end < v[0]) {
             ++ans;
             end = v[1];
         }
