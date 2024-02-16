8dde8a9ffd4f6394fd51d85d65a24777768c3509
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-16 12:52:20 +0900

    [level 3] Title: 스티커 모으기(2), Time: 1.21 ms, Memory: 7.11 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 
 int solution(vector<int> sticker) {
-    vector<int> dp(sticker.size());
-    dp[0] = sticker[0];
-    dp[1] = sticker[1];
-    dp[2] = sticker[0] + sticker[2];
+    vector<int> dp1(sticker.size()), dp2(sticker.size());
+    dp1[0] = sticker[0];
+    dp1[1] = sticker[1];
+    dp1[2] = sticker[0] + sticker[2];
     for (int i = 3; i < sticker.size() - 1; i++)
-        dp[i] = sticker[i] + max(dp[i - 2], dp[i - 3]);
-    const int ans = *max_element(dp.begin(), dp.end());
-    dp[0] = 0;
-    dp[1] = sticker[1];
-    dp[2] = sticker[2];
+        dp1[i] = max(dp1[i - 2], dp1[i - 3]) + sticker[i];
+    dp2[1] = sticker[1];
+    dp2[2] = sticker[2];
     for (int i = 3; i < sticker.size(); i++)
-        dp[i] = sticker[i] + max(dp[i - 2], dp[i - 3]);
-    return max(ans, *max_element(dp.begin(), dp.end()));
+        dp2[i] = max(dp2[i - 2], dp2[i - 3]) + sticker[i];
+    return max(*max_element(dp1.begin(), dp1.end()), *max_element(dp2.begin(), dp2.end()));
 }
