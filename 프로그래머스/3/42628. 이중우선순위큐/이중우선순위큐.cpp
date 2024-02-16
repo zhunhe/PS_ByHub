855fe2e562e8a75164e8414f89530611878127a2
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-16 15:31:30 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -2,27 +2,20 @@
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
-    priority_queue<int> pq1;
-    priority_queue<int, vector<int>, greater<int>> pq2;
-    int cnt = 0;
+    multiset<int> st;
     for (const auto& s : operations) {
         stringstream ss(s);
         string cmd; int number;
         ss >> cmd >> number;
-        if (cmd == "I") {
-            ++cnt;
-            pq1.push(number);
-            pq2.push(number);
-        } else if (cnt) {
-            --cnt;
-            number == 1 ? pq1.pop() : pq2.pop();
-        }
-        if (cnt == 0) {
-            while (!pq1.empty()) pq1.pop();
-            while (!pq2.empty()) pq2.pop();
+        if (cmd == "I")
+            st.insert(number);
+        else if (!st.empty()) {
+            if (number == 1)
+                st.erase(--st.end());
+            else
+                st.erase(st.begin());
         }
     }
-    if (cnt == 0)
-        return {0, 0};
-    return {pq1.top(), pq2.top()};
+    if (st.empty()) return {0, 0};
+    return {*(--st.end()), *st.begin()};
 }
