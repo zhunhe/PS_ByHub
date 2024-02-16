7de63ee99e4e33971c2ea17fc578dbf6f0a86d7c
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-16 15:22:41 +0900

    [level 3] Title: 기지국 설치, Time: 0.05 ms, Memory: 3.97 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int solution(int n, vector<int> stations, int w) {
     int ans = 0, s = 1;
-    const double range = 2 * w + 1;
+    const int range = 2 * w + 1;
     for (const auto station : stations) {
         ans += (station - s - w + range - 1) / range;
         s = station + w + 1;
     }
-    ans += (n - s + range) / range;
+    ans += (n - s + 1 + range - 1) / range;
     return ans;
 }
