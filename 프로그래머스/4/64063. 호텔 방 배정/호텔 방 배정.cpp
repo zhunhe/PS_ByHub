1ac3f02f8fd25e4c04de3fef5eb9bb57eb7ac234
Junhyuk Hur <xuzhunhe@gmail.com>
2024-02-27 14:44:16 +0900

    [level 4] Title: 호텔 방 배정, Time: 149.18 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map<long long, long long> m;
+
+long long find(long long n) {
+    if (m.find(n) == m.end())
+        return n;
+    return m[n] = find(m[n]);
+}
+
+vector<long long> solution(long long k, vector<long long> room_number) {
+    vector<long long> ans;
+    for (const auto n : room_number) {
+        if (m.find(n) == m.end()) {
+            ans.push_back(n);
+            m[n] = find(n + 1);
+        } else {
+            long long next = find(n);
+            ans.push_back(next);
+            m[next] = find(next + 1);
+        }
+    }
+    return ans;
+}
